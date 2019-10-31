168348852ea699799528881e17e672fe9252d16f
admiswalker <admiswalker@gmail.com>
2019-11-01 02:15:54 +0900

    add abc_087_b

---
+++ AtCoder/abc_087_b/main.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int A, B, C, X;
+    cin >> A;
+    cin >> B;
+    cin >> C;
+    cin >> X;
+    
+    int ans=0;
+    for(int a=0; a<(A+1); a++){
+        for(int b=0; b<(B+1); b++){
+            for(int c=0; c<(C+1); c++){
+                int sum = 500*a + 100*b + 50*c;
+                if(sum == X){
+                    ans++;
+                }
+            }
+        }
+    }
+    
+    cout << ans;
+    
+    return 0;
+}
