78ce8aa8fe429a90bf1c4973fd79724d9bb6e979
admiswalker <admiswalker@gmail.com>
2019-11-02 21:45:19 +0900

    add abc_049_c

--- AtCoder/abc_049_c/main.cpp
+++ AtCoder/abc_049_c/main.cpp
@@ -1,6 +1,43 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+bool cmp(const char* lhs, const char* rhs, const int len){
+    for(int i=0; i<len; ++i){
+        if(lhs[i]!=rhs[i]){ return false; }
+    }
+    return true;
+}
+
+int main(){
+    string str; cin >> str; std::reverse(str.begin(), str.end());
+    
+    string d2 = "dreamer"; // odered by greter length
+    string e2 = "eraser";
+    string d1 = "dream";
+    string e1 = "erase";
+    vector<string> vecDE = {d2, e2, d1, e1};
+    for(int i=0; i<vecDE.size(); ++i){ std::reverse(vecDE[i].begin(), vecDE[i].end()); }
+    
+    for(int i=0; i!=str.size();){
+        bool TF=false;
+        for(int v=0; v<vecDE.size(); v++){
+            if( cmp(&str[i], vecDE[v].c_str(), vecDE[v].size()) ){
+                i += vecDE[v].size();
+                TF=true;
+                break;
+            }
+        }
+        if(TF==false){ cout << "NO"; return 0; }
+    }
+    cout << "YES"; 
+    
+    return 0;
+}
+
+/*
+#include <bits/stdc++.h>
+using namespace std;
+
 int main(){
     string str; cin >> str; std::reverse(str.begin(), str.end());
     
@@ -29,3 +66,4 @@ int main(){
     
     return 0;
 }
+*/
