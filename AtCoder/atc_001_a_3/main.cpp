ea1e65c4d834356ddcbb7d5caa8a0b69cb79712e
admiswalker <admiswalker@gmail.com>
2019-11-18 20:55:49 +0900

    vector<vector<struct XY>> to vector<string>

--- AtCoder/atc_001_a_3/main.cpp
+++ AtCoder/atc_001_a_3/main.cpp
@@ -1,49 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct XY{
-    XY(int x_in, int y_in){ x=x_in; y=y_in; }
-    ~XY(){}
-    int x;
-    int y;
+struct RowCol{
+    RowCol(int r_in, int c_in){ r=r_in; c=c_in; }
+    ~RowCol(){}
+    int r;
+    int c;
 };
 
-struct elem{
-    elem(){ reached=false; }
-    ~elem(){}
-    char ch;
-    bool reached;
-};
+int vec_dr[] = {1, 0, -1,  0};
+int vec_dc[] = {0, 1,  0, -1};
 
-template<class T>
-void vvec_alloc(vector<vector<T>>& mat, int H, int W){
-    mat.resize(H);
-    for(int r=0; r<H; ++r){
-        mat[r].resize(W);
-    }
-}
-
-int vec_dx[] = {1, 0, -1,  0};
-int vec_dy[] = {0, 1,  0, -1};
-
-bool search(vector<vector<struct elem>>& mat, int x_begin, int y_begin){
-    vector<XY> vecXY;
-    vecXY.push_back( XY(x_begin, y_begin) );
-    while(vecXY.size()!=0){
-        struct XY xy = vecXY.back();
-        vecXY.pop_back();
+bool search(vector<string>& mat, int r_begin, int c_begin){
+    vector<struct RowCol> vecRC;
+    vecRC.push_back( RowCol(r_begin, c_begin) );
+    while(vecRC.size()!=0){
+        struct RowCol rc = vecRC.back();
+        vecRC.pop_back();
         
         for(int i=0; i<4; ++i){
-            int dy = xy.y + vec_dx[i];
-            int dx = xy.x + vec_dy[i];
-            if(dy<0 || dy>=mat.size() || dx<0 || dx>=mat[dy].size()){ continue; }
+            int dr = rc.r + vec_dr[i];
+            int dc = rc.c + vec_dc[i];
+            if(dr<0 || dr>=mat.size() || dc<0 || dc>=mat[dr].size()){ continue; }
             
-            if(mat[dy][dx].reached){ continue; }
-            mat[dy][dx].reached = true;
-            
-            if(mat[dy][dx].ch == '#'){ continue; }
-            if(mat[dy][dx].ch == 'g'){ return true; }
-            vecXY.push_back( XY(dx, dy) );
+            if(mat[dr][dc] == '#'){ continue; }
+            if(mat[dr][dc] == 'g'){ return true; }
+            mat[dr][dc] = '#';
+            vecRC.push_back( RowCol(dr, dc) );
         }
     }
     return false;
@@ -51,20 +34,17 @@ bool search(vector<vector<struct elem>>& mat, int x_begin, int y_begin){
 
 int main(){
     int H, W; cin >> H >> W;
-    vector<vector<struct elem>> mat; // row-major
-    vvec_alloc(mat, H, W);
-    
-    int x, y;
+	vector<string> mat(H);
+	
+    int r_begin, c_begin;
     for(int r=0; r<mat.size(); ++r){
-        string buf;
-        cin >> buf;
+        cin >> mat[r];
         for(int c=0; c<mat[r].size(); ++c){
-            mat[r][c].ch = (char)buf[c];
-            if(mat[r][c].ch=='s'){ y=r; x=c; }
+            if(mat[r][c]=='s'){ r_begin=r; c_begin=c; }
         }
     }
     
-    bool ret = search(mat, x, y);
+    bool ret = search(mat, r_begin, c_begin);
     
     cout << (ret ? "Yes":"No") << endl;
     
