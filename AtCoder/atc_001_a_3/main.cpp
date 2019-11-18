6aa2cc05edb4b6fad5226a10a6afdf0b9f9f0644
admiswalker <admiswalker@gmail.com>
2019-11-18 21:01:59 +0900

    rm self defined struct

--- AtCoder/atc_001_a_3/main.cpp
+++ AtCoder/atc_001_a_3/main.cpp
@@ -1,32 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct RowCol{
-    RowCol(int r_in, int c_in){ r=r_in; c=c_in; }
-    ~RowCol(){}
-    int r;
-    int c;
-};
-
 int vec_dr[] = {1, 0, -1,  0};
 int vec_dc[] = {0, 1,  0, -1};
 
 bool search(vector<string>& mat, int r_begin, int c_begin){
-    vector<struct RowCol> vecRC;
-    vecRC.push_back( RowCol(r_begin, c_begin) );
+    vector<pair<int,int>> vecRC;
+    vecRC.push_back( pair<int,int>(r_begin, c_begin) );
     while(vecRC.size()!=0){
-        struct RowCol rc = vecRC.back();
+        pair<int,int> rc = vecRC.back();
         vecRC.pop_back();
         
         for(int i=0; i<4; ++i){
-            int dr = rc.r + vec_dr[i];
-            int dc = rc.c + vec_dc[i];
+            int dr = rc.first  + vec_dr[i];
+            int dc = rc.second + vec_dc[i];
             if(dr<0 || dr>=mat.size() || dc<0 || dc>=mat[dr].size()){ continue; }
             
             if(mat[dr][dc] == '#'){ continue; }
             if(mat[dr][dc] == 'g'){ return true; }
             mat[dr][dc] = '#';
-            vecRC.push_back( RowCol(dr, dc) );
+            vecRC.push_back( pair<int,int>(dr, dc) );
         }
     }
     return false;
