81bbe61b9643f683e0feb062779b1db21e67ae60
admiswalker <admiswalker@gmail.com>
2019-11-24 22:30:21 +0900

    joi_2007_a

--- AtCoder/joi_2007_a/main.cpp
+++ AtCoder/joi_2007_a/main.cpp
@@ -1,11 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int vec_sum(const vector<int>& vec){
+    int sum=0;
+    for(int i=0; i<vec.size(); ++i){
+        sum += vec[i];
+    }
+    return sum;
+}
+
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     
-    printf("hello\n");
+    int payment; cin >> payment;
+    int change = 1000 - payment;
+    
+    vector<int> vecCoin = {500, 100, 50, 10, 5, 1};
+    vector<int> vecNum(vecCoin.size(), 0);
+    
+    int sum=0;
+    for(int i=0; i<vecCoin.size(); ++i){
+        while(sum<change){
+            sum += vecCoin[i]; ++vecNum[i];
+        }
+        if(sum==change){ break; }
+        sum -= vecCoin[i]; --vecNum[i];
+    }
+    
+    int count = vec_sum(vecNum);
+    cout << count << endl;
     
     return 0;
 }
