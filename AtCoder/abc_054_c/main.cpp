e0f65ad16aa9f149a1c6e24f719743099fd6a14c
admiswalker <admiswalker@gmail.com>
2019-11-24 18:05:06 +0900

    abc_054_c

--- AtCoder/abc_054_c/main.cpp
+++ AtCoder/abc_054_c/main.cpp
@@ -2,16 +2,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void search(int& sum, vector<vector<int>>& mat, vector<int>& vecUsed, int r, int steps){
-    int N = mat.size();
-    if(steps == N){ sum++; return; }
+void search(int& sum, const vector<vector<int>>& mat, vector<int>& vecIsUsed, int r, int steps){
+    if(steps == mat.size()-1){ ++sum; return; }
     
-    for(int i=1; i<N; ++i){
-        if(mat[r][i]==1 && vecUsed[i]==0){
-            vecUsed[i] = 1;
-            search(sum, mat, vecUsed, i, steps+1);
-            vecUsed[i] = 0;
-        }
+    for(int i=1; i<mat.size(); ++i){
+        if(mat[r][i]==0 || vecIsUsed[i]==1){ continue; }
+        vecIsUsed[i] = 1;
+        search(sum, mat, vecIsUsed, i, steps+1);
+        vecIsUsed[i] = 0;
     }
     return;
 }
@@ -24,94 +22,17 @@ int main(){
     cin >> N >> M;
     vector<vector<int>> mat(N, vector<int>(N, 0));
     
-    for(int i=0; i<N; i++){
-        int a; cin >> a; a--;
-        int b; cin >> b; b--;
+    for(int i=0; i<M; ++i){
+        int a; cin >> a; --a;
+        int b; cin >> b; --b;
         mat[a][b] = 1;
         mat[b][a] = 1;
     }
     
-    vector<int> vecUsed(N, 0);
+    vector<int> vecIsUsed(N, 0);
     int sum=0;
-    search(sum, mat, vecUsed, 0, 1);
+    search(sum, mat, vecIsUsed, 0, 0);
     cout << sum << endl;
     
     return 0;
 }
-
-/*
-#include <bits/stdc++.h>
-using namespace std;
-
-// use mat as col-major
-#define MatC(mat, r, c) mat[c][r]
-
-vector<int> col2vecRow(const vector<vector<int>>& mat, int col_idx){
-    vector<int> ret;
-    for(int r=0; r<mat[0].size(); ++r){
-        if( MatC(mat, r, col_idx)==1 ){
-            ret.emplace_back(r);
-        }
-    }
-    return ret;
-}
-
-int count_path(const vector<vector<int>>& mat_in){
-    int sum=0;
-    int end=mat_in.size()-1;
-//    int end=mat_in.size();
-    
-    vector<tuple<vector<vector<int>>,int,int>> vecMatCN; // col index, num
-    vector<int> vecR = col2vecRow(mat_in, 0);
-    
-    vector<vector<int>> matBuf = mat_in;
-    for(int i=0; i<vecR.size(); ++i){
-        MatC(matBuf, vecR[i], 0) = 0;
-    }
-    
-    for(int i=0; i<vecR.size(); ++i){
-        vecMatCN.emplace_back( make_tuple(matBuf, vecR[i], 0) );
-    }
-    
-    while(!vecMatCN.empty()){
-        vector<vector<int>> mat;
-        int col, num;
-        tie(mat, col, num) = vecMatCN.back(); vecMatCN.pop_back();
-        if(num==end){ sum++; continue; }
-        
-        vector<int> vecR = col2vecRow(mat, col);
-        vector<vector<int>> matBuf = mat;
-        for(int i=0; i<vecR.size(); ++i){
-            MatC(matBuf, vecR[i], 0) = 0;
-        }
-        
-        for(int i=0; i<vecR.size(); ++i){
-            vecMatCN.emplace_back( make_tuple(matBuf, vecR[i], num+1) ); // あー，，，たぶんこの辺りループしている．（一度来たという印がないから．
-        }
-    }
-    
-    return sum;
-}
-
-int main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    
-    int N, M;
-    cin >> N >> M;
-    vector<vector<int>> mat(N, vector<int>(N, 0));
-    for(int i=0; i<N; ++i){
-        int a, b;
-        cin >> a >> b;
-        a--;
-        b--;
-        MatC(mat, a, b) = 1;
-        MatC(mat, b, a) = 1;
-    }
-    
-    int paths = count_path(mat);
-    cout << paths << endl;
-    
-    return 0;
-}
-//*/
