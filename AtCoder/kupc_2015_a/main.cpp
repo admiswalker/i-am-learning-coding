36a5b730d7e805b815f015d98710053bcb163a98
admiswalker <admiswalker@gmail.com>
2019-11-25 22:11:32 +0900

    kupc_2015_a

--- AtCoder/kupc_2015_a/main.cpp
+++ AtCoder/kupc_2015_a/main.cpp
@@ -15,7 +15,7 @@ int search(const string& target, const vector<string>& vecWord){
     
     for(int i=0; i<target.size(); ++i){
         for(int n=0; n<vecWord.size(); ++n){
-            if(isEqual(&target[i], target.size()-1, vecWord[n])){ ++num; i+=vecWord[n].size()-1; break; }
+            if(isEqual(&target[i], target.size()-i+1, vecWord[n])){ ++num; i+=vecWord[n].size()-1; break; }
         }
     }
     
