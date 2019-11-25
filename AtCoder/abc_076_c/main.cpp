310cdb896403497be14198574489b305709396aa
admiswalker <admiswalker@gmail.com>
2019-11-26 07:34:51 +0900

    abc_076_c udc

--- AtCoder/abc_076_c/main.cpp
+++ AtCoder/abc_076_c/main.cpp
@@ -1,11 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+struct place{
+    int begin;
+    int len;
+};
+
+vector<struct place> str2vec_X(const string str& str, const char c){
+    vector<struct place> ret;
+    
+    for(int i=0; i<str.size()-1; ++i){
+        if(str[i]!=c){ continue; }
+        
+        struct place buf;
+        buf.begin = i;
+        for(buf.len=0; l<str.size()-1; ++buf.len){
+            if(str[i]!=str[i+1]){ break; }
+        }
+    }
+}
+
+int vecPlace2maxLen(){
+}
+
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-
     
+    string Sd; cint >> Sd;
+    string T; cint >> T;
+    
+    vector<tuple<int,int>> = str2vec_X(string str& str, const char c);
     
     return 0;
 }
