48748f4343930b44c5c578675c349f6bce0405d4
admiswalker <admiswalker@gmail.com>
2019-11-27 22:27:29 +0900

    abc_076_c miss

--- AtCoder/abc_076_c/main.cpp
+++ AtCoder/abc_076_c/main.cpp
@@ -1,36 +1,75 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct place{
-    int begin;
-    int len;
-};
-
-vector<struct place> str2vec_X(const string str& str, const char c){
-    vector<struct place> ret;
+tuple<vector<int>, vector<int>> str2vecBL(const string& str, const char c){
+    vector<int> vecBegin, vecLen;
     
-    for(int i=0; i<str.size()-1; ++i){
+    for(int i=0; i<str.size(); ++i){
         if(str[i]!=c){ continue; }
         
-        struct place buf;
-        buf.begin = i;
-        for(buf.len=0; l<str.size()-1; ++buf.len){
-            if(str[i]!=str[i+1]){ break; }
+        int begin=i;
+        int len  =0;
+        for(; i<str.size(); ++i, ++len){
+            if(str[i]!=c){ break; }
         }
+        vecBegin.emplace_back(begin);
+        vecLen.emplace_back(len);
     }
+    return make_tuple(vecBegin, vecLen);
 }
 
-int vecPlace2maxLen(){
+template<typename T>
+int argmax(const vector<T>& rhs){
+    if(rhs.size()==0){ return -1; }
+    
+    int idx=0;
+    for(int i=1; i<rhs.size(); ++i){
+        if(rhs[i]<rhs[idx]){ continue; }
+        idx = i;
+    }
+    return idx;
+}
+
+void replace(string& str, const char target, const char replacing){
+    for(int i=0; i<str.size(); ++i){
+        if( str[i]!=target ){ continue; }
+        str[i] = replacing;
+    }
 }
 
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     
-    string Sd; cint >> Sd;
-    string T; cint >> T;
+    string Sd; cin >> Sd;
+    string T; cin >> T;
     
-    vector<tuple<int,int>> = str2vec_X(string str& str, const char c);
+    vector<int> vecBegin, vecLen;
+    tie(vecBegin, vecLen) = str2vecBL(Sd, '?');
+    printf("vecBegin.size(): %lu\n", vecBegin.size());
+    for(int i=0; i<vecBegin.size(); ++i){
+        cout << vecBegin[i] << endl;
+    }
+    printf("vecLen.size(): %lu\n",   vecLen.size());
+    for(int i=0; i<vecLen.size(); ++i){
+        cout << vecLen[i] << endl;
+    }
+    
+    int maxIdx = argmax(vecLen);
+    printf("madIdx: %d\n", maxIdx);
+    if(vecLen[maxIdx] != T.size()){
+        cout << "UNRESTORABLE" << endl;
+        return 0;
+    }
+    
+    int begin = vecBegin[maxIdx];
+    for(int i=0; i<T.size(); ++i){
+        Sd[begin+i] = T[i];
+    }
+    
+    replace(Sd, '?', 'a');
+    
+    cout << Sd << endl;
     
     return 0;
 }
