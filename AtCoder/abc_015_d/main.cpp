58e20465734da880e705fd4c910adb5908bd30b0
admiswalker <admiswalker@gmail.com>
2019-12-01 14:27:56 +0900

    abc_015_d udc

---
+++ AtCoder/abc_015_d/main.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct sAB{
+    int width;
+    int imp;   // importance
+};
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int W;    cin >> W;
+    int N, K; cin >> N >> K;
+    vector<struct sAB> vecAB(N);
+    for(int i=0; i<vecAB.size(); ++i){
+        cin >> vecAB[i].width >> vecAB[i].imp;
+    }
+    
+    vector<int> vecImp;
+    int impBuf=0;
+    for(int k=0; k<K; ++k){
+        for(){
+        }
+    }
+    
+    
+    
+    //printf("h\n");
+    
+    return 0;
+}
