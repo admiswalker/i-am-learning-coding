0ab491e18705cbac9f316ab5ee7f1ad1c7bab973
admiswalker <admiswalker@gmail.com>
2019-12-01 21:05:53 +0900

    abc_015_d TLE

--- AtCoder/abc_015_d/main.cpp
+++ AtCoder/abc_015_d/main.cpp
@@ -2,50 +2,51 @@
 using namespace std;
 
 template<typename T>
-int64_t maxVal(const vector<T>& rhs){
+T maxVal(const vector<T>& rhs){
     T lhs=rhs[0];
-    for(int64_t i=1; i<rhs.size(); ++i){
+    for(uint64_t i=1; i<rhs.size(); ++i){
         if(rhs[i]>lhs){ lhs=rhs[i]; }
     }
     return lhs;
 }
 
 struct sAB{
-    int64_t width;
-    int64_t imp;   // importance
+    uint64_t width;
+    uint64_t imp;   // importance
 };
 
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     
-    int64_t W;    cin >> W;
-    int64_t N, K; cin >> N >> K;
+    uint64_t W;    cin >> W;
+    uint64_t N, K; cin >> N >> K;
     vector<struct sAB> vecAB(N);
-    for(int64_t i=0; i<vecAB.size(); ++i){
+    for(uint64_t i=0; i<vecAB.size(); ++i){
         cin >> vecAB[i].width >> vecAB[i].imp;
     }
     
-    int64_t numChoices = 1 << vecAB.size();
+    uint64_t numChoices = (uint64_t)1 << vecAB.size();
     
-    vector<int64_t> vecImp;
-    int64_t impBuf=0;
-    for(int64_t n=0; n<numChoices; ++n){
-        int64_t num=0;
-        int64_t width=0;
-        int64_t imp=0;
-        for(int64_t i=0; i<vecAB.size(); ++i){
-            if( !(n&(1<<i)) ){ continue; }
+    uint64_t ans=0;
+    for(uint64_t n=0; n<numChoices; ++n){
+        uint64_t num=0;
+        uint64_t width=0;
+        uint64_t imp=0;
+        for(uint64_t i=0; i<vecAB.size(); ++i){
+            if( !(n&((uint64_t)1<<i)) ){ continue; }
             ++num;
+            if(num>K){ break; }
             width += vecAB[i].width;
             imp   += vecAB[i].imp;
         }
         if(num>K){ continue; }
         if(width>W){ continue; }
-        vecImp.emplace_back(imp);
+        
+        if(ans<imp){ ans=imp; }
     }
     
-    cout << maxVal(vecImp) << endl;
+    cout << ans << endl;
     
     return 0;
 }
