5f378fba1cd5487471aca4c3446495c7c4897b4e
admiswalker <admiswalker@gmail.com>
2019-12-12 22:16:44 +0900

    add

--- AtCoder/abc_032_d/main05_hashT.cpp
+++ AtCoder/abc_032_d/main05_hashT.cpp
@@ -44,8 +44,7 @@ inline std::size_t Hash::operator()(const struct VW& rhs) const {
 }
 //*/
 
-//int64_t dps(const vector<struct VW>& vecVW, unsigned int i, int64_t w){
-int64_t dps(unordered_map<struct VW, int>& hT, const vector<struct VW>& vecVW, unsigned int i, int64_t w){
+int64_t dps(unordered_map<struct VW, int64_t>& hT, const vector<struct VW>& vecVW, unsigned int i, int64_t w){
     if(i==vecVW.size()){ return 0; }
     
     auto itr = hT.find( VW(i, w) );
@@ -55,10 +54,8 @@ int64_t dps(unordered_map<struct VW, int>& hT, const vector<struct VW>& vecVW, u
     int64_t w_minusW = w - vecVW[i].w;
     if(w_minusW<0){
         val = dps(hT, vecVW, i+1, w);
-//        val = dps(vecVW, i+1, w);
     }else{
         val = max(dps(hT, vecVW, i+1, w_minusW)+vecVW[i].v, dps(hT, vecVW, i+1, w));
-//        val = max(dps(vecVW, i+1, w_minusW)+vecVW[i].v, dps(vecVW, i+1, w));
     }
     
     hT.insert( pair<struct VW,int64_t>(VW(i,w),val) );
@@ -74,10 +71,9 @@ int main(){
     vector<struct VW> vecVW(N);
     for(unsigned int i=0; i<vecVW.size(); ++i){ cin >> vecVW[i].v >> vecVW[i].w; }
     
-    unordered_map<struct VW, int> hashT;
 //    unordered_map<struct VW, int, Hash> hashT;
+    unordered_map<struct VW, int64_t> hashT;
     cout << dps(hashT, vecVW, 0, (int64_t)W) << endl;
-//    cout << dps(vecVW, 0, (int64_t)W) << endl;
     
     return 0;
 }
