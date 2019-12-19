1a342645b3c5f15741d2821f45974e5425fa7240
admiswalker <admiswalker@gmail.com>
2019-12-19 23:53:34 +0900

    rm meaning less command

--- AtCoder/abc_032_d/main09.cpp
+++ AtCoder/abc_032_d/main09.cpp
@@ -13,27 +13,6 @@ template <typename T> inline std::vector<T> sorted   (const std::vector<T>& rhs_
 template <typename T> inline void           sort_gr  (      std::vector<T>& rhs   ){                            std::sort(rhs.begin(), rhs.end(), std::greater<T>());             } // Descending: 降順: 9, 8, 7, ...
 template <typename T> inline std::vector<T> sorted_gr(const std::vector<T>& rhs_in){ std::vector<T> rhs=rhs_in; std::sort(rhs.begin(), rhs.end(), std::greater<T>()); return rhs; } // Descending: 降順: 9, 8, 7, ...
 
-/*
-template<typename T>
-int64_t sum(const vector<T>& vecRhs){
-    int64_t sum=0ll;
-    for(unsigned int i=0; i<vecRhs.size(); ++i){
-        sum += (int64_t)vecRhs[i];
-    }
-    return sum;
-}
-
-
-template<typename T>
-T max(const vector<T>& vecRhs){
-    T lhs = vecRhs[0];
-    for(unsigned int i=1; i<vecRhs.size(); ++i){
-        if(lhs<=vecRhs[i]){ continue; }
-        lhs = vecRhs[i];
-    }
-    return lhs;
-}
-//*/
 //---
 
 struct VW{
@@ -63,18 +42,6 @@ double estimate(const vector<struct VW>& vecVW, int64_t i, int64_t v_cur, int64_
     
     return estV;
 }
-/*
-double estimate_ProvisionalSolution(const vector<struct VW>& vecVW, int W){
-    double estV=0.0;
-    int64_t w_sum=0;
-    
-    for(int i=0; i<vecVW.size() && (w_sum+vecVW[i].w)<=W; ++i){
-        estV  += (double)vecVW[i].v;
-        w_sum += vecVW[i].w;
-    }
-    return estV;
-}
-//*/
 
 // branchAndBound
 void bab(const vector<struct VW>& vecVW, unsigned int i, int64_t& v_max, int64_t v_cur, int64_t w_cur){
@@ -107,8 +74,6 @@ int main(){
     
     sort_gr(vecVW);
     
-//    double est_max = estimate_ProvisionalSolution(vecVW, W);
-//    int64_t ans = bab(vecVW, est_max, est_max, 0, 0, W);
     int64_t v_max = 0;
     bab(vecVW, 0, v_max, 0, W);
     cout << v_max << endl;
