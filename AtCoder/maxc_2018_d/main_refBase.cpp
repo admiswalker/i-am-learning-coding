c0e7e0b4bf1774026c8dab96f617eea07d764afd
admiswalker <admiswalker@gmail.com>
2019-12-25 17:40:21 +0900

    maxc_2018_d udi

--- AtCoder/maxc_2018_d/main_refBase.cpp
+++ AtCoder/maxc_2018_d/main_refBase.cpp
@@ -3,6 +3,17 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+//---
+
+template <typename T> inline void           sort     (      std::vector<T>&  rhs   ){                            std::sort(rhs.begin(), rhs.end());             }                    // Ascending: 昇順: 0, 1, 2, ...
+template <typename T> inline std::vector<T> sorted   (const std::vector<T>&  rhs_in){ std::vector<T> rhs=rhs_in; std::sort(rhs.begin(), rhs.end()); return rhs; }                    // Ascending: 昇順: 0, 1, 2, ...
+template <typename T> inline std::vector<T> sorted   (      std::vector<T>&& rhs   ){                            std::sort(rhs.begin(), rhs.end()); return rhs; }                    // Ascending: 昇順: 0, 1, 2, ...
+template <typename T> inline void           sort_gr  (      std::vector<T>&  rhs   ){                            std::sort(rhs.begin(), rhs.end(), std::greater<T>());             } // Descending: 降順: 9, 8, 7, ...
+template <typename T> inline std::vector<T> sorted_gr(const std::vector<T>&  rhs_in){ std::vector<T> rhs=rhs_in; std::sort(rhs.begin(), rhs.end(), std::greater<T>()); return rhs; } // Descending: 降順: 9, 8, 7, ...
+template <typename T> inline std::vector<T> sorted_gr(      std::vector<T>&& rhs   ){                            std::sort(rhs.begin(), rhs.end(), std::greater<T>()); return rhs; } // Descending: 降順: 9, 8, 7, ...
+
+//---
+
 int main(){
     cin.tie(0);
     ios::sync_with_stdio(false);
@@ -26,21 +37,18 @@ int main(){
     for(int i=1; i<M; ++i){
         if( !place2laps[i].size() ){ continue; }
         
-        sort(place2laps[i].begin(), place2laps[i].end());
+        sort(place2laps[i]);
         
         vector<pair<int, int>> V2;
-        int k1 = 0; // place
-        int k2 = 0; // numOfLaps
+        int place = 0; // place
+        int laps  = 0; // numOfLaps
         for(unsigned int j=0; j<place2laps[i].size(); ++j){
-            int laps = place2laps[i][j];
-            k1 += i;
-            k2 += laps;
-            if (k1 >= M) {
-                k1 -= M;
-                k2++;
-            }
-            if (k1 == 0) continue;
-            V2.push_back( make_pair(k1, k2) );
+            place += i;
+            laps  += place2laps[i][j];
+            if(place >= M){ place -= M; laps++; }
+            
+//            if(place == 0){ continue; }
+            V2.push_back( make_pair(place, laps) );
         }
         vector<pair<int, int>> V3;
         for(int j=0; j<M; ++j){
