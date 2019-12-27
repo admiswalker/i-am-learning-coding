18dbbf62f9155e20f6890bf16968b2e744b64e63
admiswalker <admiswalker@gmail.com>
2019-12-27 16:47:00 +0900

    udi (change loop)

--- AtCoder/maxc_2018_d/main02.cpp
+++ AtCoder/maxc_2018_d/main02.cpp
@@ -60,7 +60,7 @@ bool dps_internal(const vector<int>& place2laps, int N, int M, int L, int X){
 //---
 
 bool dps(const vector<int>& vecA, int N, int M, int L, int X){
-    
+    /*
     vector<vector<int>> place2laps(M);
     for(int i=0; i<N; ++i){
         int div = vecA[i] / M;
@@ -85,24 +85,40 @@ bool dps(const vector<int>& vecA, int N, int M, int L, int X){
             vecPL.push_back( make_pair(place, laps) );
         }
     }
+    //*/
+    
+    vector<pair<int, int>> vecPL(N);
+    for(int i=0; i<N; ++i){
+        int div = vecA[i] / M;
+        int mod = vecA[i] - M*div; // same as a "a % M"
+        vecPL[i] = make_pair(mod, div);
+    }
     
     vector<int> dp_min (M, INT_MAX); dp_min [0] = 0;
     vector<int> dp_prev(M, INT_MAX); dp_prev[0] = 0;
     
-    for(int p=0; p<M; ++p){
-        if(dp_prev[p] == INT_MAX){ continue; }
-        
-        for(unsigned int i=0; i<vecPL.size(); ++i){
+    for(unsigned int i=0; i<vecPL.size(); ++i){
+        for(int p=0; p<M; ++p){
+            if(dp_prev[p] == INT_MAX){ continue; }
+            
             int place, laps;
             tie(place, laps) = vecPL[i];
             if(p+place >= M){ place-=M; ++laps; }
             
             dp_min[p+place] = min(dp_min[p+place], dp_prev[p]+laps);
         }
-        
         if(dp_min[L] < X){ return true; }
         dp_prev = dp_min;
     }
+    /*
+    for(int i=0; i<N; ++i){
+        for(int l=0; l<=len; ++l){
+            if      (dp[l]>=0                     ){ dp[l] = 1;
+            }else if(l<vecA[i] || dp[l-vecA[i]]<=0){ dp[l] = -1;
+            }else                                  { dp[l] = dp[l-vecA[i]] - 1; }
+        }
+    }
+    //*/
     return false;
 }
 
