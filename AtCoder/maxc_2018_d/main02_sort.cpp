fa6cc6423e53e8d57fc41f622c857d86d1628601
admiswalker <admiswalker@gmail.com>
2019-12-27 23:19:03 +0900

    fix bugs (while AC but my code has bug, when required same mod twiceor more)

--- AtCoder/maxc_2018_d/main02_sort.cpp
+++ AtCoder/maxc_2018_d/main02_sort.cpp
@@ -21,10 +21,6 @@ public:
     ~divMod(){}
     
     bool operator<(const struct divMod& rhs){
-//        if      ( (*this).div <  rhs.div ){ return true;
-//        }else if( (*this).div == rhs.div ){ return (*this).mod < rhs.mod;
-//        }else                             { return false;
-//        }
         if      ( (*this).mod <  rhs.mod ){ return true;
         }else if( (*this).mod == rhs.mod ){ return (*this).div < rhs.div;
         }else                             { return false;
@@ -42,26 +38,23 @@ bool dps(const vector<int>& vecA, int N, int M, int L, int X){
         vecPL[i].mod = mod;
     }
     sort( vecPL );
-//    for(unsigned int i=0; i<vecPL.size(); ++i){
-//        printf("%d: ", vecPL[i].div);
-//        printf("%d", vecPL[i].mod);
-//        printf("\n");
-//    }
     
     vector<int> dp_min (M, INT_MAX); dp_min [0] = 0;
     vector<int> dp_prev(M, INT_MAX); dp_prev[0] = 0;
     
-//    vector<int> vecNum(N); for(int i=0; i<vecNum.size(); ++i){ vecNum[i]=i; }
     for(unsigned int i=0; i<vecPL.size(); ++i){
+        int place=0, laps=0;
         for(int p=0; p<M; ++p){
             if(dp_prev[p] == INT_MAX){ continue; }
             
-            int place = p + vecPL[i].mod;
-            int laps  =     vecPL[i].div;
+            place += p + vecPL[i].mod;
+            laps  +=     vecPL[i].div;
             if(place >= M){ place-=M; ++laps; }
             
             dp_min[place] = min(dp_min[place], dp_prev[p]+laps);
-            if(i<vecPL.size()-1 && vecPL[i].mod==vecPL[i+1].mod){ i++; p--; continue; } // same mod has no meaning in its order. so we can process it in same time.
+            
+            if(i<vecPL.size()-1 && vecPL[i].mod==vecPL[i+1].mod){ i++; p--; continue; } // There is no difference in vecPL[i] and vecPL[i+1] with the same mod value is number of laps, 
+            place=0; laps=0;                                                            // when beginnging from same p. Because both of it will move on same distance of mod.
         }
         if(dp_min[L] < X){ return true; }
         dp_prev = dp_min;
