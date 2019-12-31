fa5c78f6c2efe388155193e201fc4744a9b37ddd
admiswalker <admiswalker@gmail.com>
2019-12-31 18:35:17 +0900

    udi

--- AtCoder/abc_006_d/main03.cpp
+++ AtCoder/abc_006_d/main03.cpp
@@ -7,41 +7,21 @@ typedef unsigned int uint;
 int dps(const vector<int>& vecA){
     const uint& N = vecA.size();
     
-    //*
-    vector<int> dp(vecA.size(), INT_MAX); dp[0]=vecA[0];
+    vector<int> dp(vecA.size(), INT_MAX); //dp[0]=vecA[0];
     
     for(uint i=0; i<N; ++i){
         for(uint j=0; j<=i; ++j){
-            if(dp[j] < vecA[i]){
-                dp[j] = min(dp[j], vecA[i]);
-            }
-        }
-    }
-    //*/
-    /*
-    for(uint i=0; i<N; ++i){
-        for(uint j=0; j<=i; ++j){
             if(i==0 || dp[i-1] < vecA[j]){
-                dp[j] = min(dp[i], vecA[j]);
-            }
-        }
-    }//*/
-    /*
-    vector<int> dp(vecA.size(), INT_MAX);
-    for(uint i=1; i<N; ++i){
-        for(uint j=0; j<=i; ++j){
-            if(dp[i-1] < vecA[j]){
                 dp[i] = min(dp[i], vecA[j]);
             }
         }
     }
-    //*/
     
     for(uint i=1; i<dp.size(); ++i){
         printf("i: %d, %d\n", i, dp[i]);
         if(dp[i]==INT_MAX){ return i; }
     }
-    return 1;
+    return dp.size();
 }
 
 int main(){
