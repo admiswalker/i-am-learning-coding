8a4bbee92eb7e7fc66e5e3207a4e5674c0bff069
admiswalker <admiswalker@gmail.com>
2020-01-05 23:52:43 +0900

    udi

--- AtCoder/dwacon_04_c/main_refBase.cpp
+++ AtCoder/dwacon_04_c/main_refBase.cpp
@@ -2,47 +2,63 @@
 
 #include <bits/stdc++.h>
 using namespace std;
+typedef unsigned int uint;
 typedef int64_t int64;
+
+int sum(const vector<int>& rhs){
+    int lhs = rhs[0];
+    for(unsigned int i=1; i<rhs.size(); ++i){ lhs+=rhs[i]; }
+    return lhs;
+}
 
 int main(){
     const int64 MOD=1e9+7;
     int N, M;
     cin >> N >> M;
-
-    vector<int> vecA(N+1); vecA[0]=-1;
-    vector<int> vecB(M+1); vecB[0]=-1;
-//    int a[101], b[101];
-//    a[0]=-1, b[0]=-1;
+    
+    vector<int> vecA(N); //vecA[0]=-1;
+    vector<int> vecB(M); //vecB[0]=-1;
+    for(uint i=0; i<vecA.size(); ++i){ cin >> vecA[i]; }
+    for(uint i=0; i<vecB.size(); ++i){ cin >> vecB[i]; }
+    
     int cta[101], ctb[101];
-    int sa=0, sb=0;
+//    int sa=0, sb=0;
     int ct;
-    for(int i=1; i<=N; i++){
-//        cin>>a[i]; sa+=a[i];
-        cin >> vecA[i]; sa+=vecA[i];
+    
+    ct=1;
+    int sum_a = sum(vecA);
+    for(int i=1; i<N; i++){
+//    for(int i=1; i<=N; i++){
+//        sum_a += vecA[i];
         if(vecA[i]!=vecA[i-1]) ct=1;
         cta[i]=ct;
         ct++;
     }
-    for(int i=1; i<=M; i++){
-        cin>>vecB[i]; sb+=vecB[i];
+    
+    ct=1;
+    int sum_b = sum(vecB);
+    for(int i=1; i<M; i++){
+//    for(int i=1; i<=M; i++){
+//        sb+=vecB[i];
         if(vecB[i]!=vecB[i-1]) ct=1;
         ctb[i]=ct;
         ct++;
     }
+    
     int64 dpa[1001]={}, dpb[1001]={};
     dpa[0]=1, dpb[0]=1;
     for(int i=1; i<=N; i++){
-        for(int j=cta[i]; j<=sb; j++){
-            dpa[j]+=dpa[j-cta[i]];
+        for(int j=cta[i]; j<=sum_b; j++){
+            dpa[j]+=dpa[j-cta[i-1]];
             dpa[j]%=MOD;
         }
     }
     for(int i=1; i<=M; i++){
-        for(int j=ctb[i]; j<=sa; j++){
-            dpb[j]+=dpb[j-ctb[i]];
+        for(int j=ctb[i]; j<=sum_a; j++){
+            dpb[j]+=dpb[j-ctb[i-1]];
             dpb[j]%=MOD;
         }
     }
-    cout<<dpa[sb]*dpb[sa]%MOD<<endl;
+    cout<<dpa[sum_b]*dpb[sum_a]%MOD<<endl;
     return 0;
 }
