c7be275c1cb067b24c46830a45c57153566d7a8b
admiswalker <admiswalker@gmail.com>
2020-01-08 22:51:49 +0900

    add main02_origBase.cpp

---
+++ AntsBook/2_3_7/main02_origBase.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void dps(const int n, const int m, const int mod){
+    vector<vector<int>> dp(m+1, vector<int>(n+1));
+    dp[0][0] = 1;
+    for(int i=1; i<=m; ++i){
+        for(int j=0; j<=n; ++j){
+            if(j-i >= 0){
+                dp[i][j] = (dp[i-1][j] + dp[i][j-i]) % mod;
+            }else{
+                dp[i][j] = dp[i-1][j] % mod;
+            }
+        }
+    }
+    printf("%d\n", dp[m][n]);
+}
+
+int main(){
+    int n=4;
+    int m=3;
+    int mod=10000;
+    
+    dps(n, m, mod);
+    
+    return 0;
+}
+
+/*
+4
+3
+10000
+*/
