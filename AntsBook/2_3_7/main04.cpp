7e684a71aab9c6c1e7e7ab695365564b9142d219
admiswalker <admiswalker@gmail.com>
2020-01-12 14:50:46 +0900

    add AntsBook_2_3_7_main04.cpp

---
+++ AntsBook/2_3_7/main04.cpp
@@ -0,0 +1,34 @@
+// ref of base: ants book page. 66
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void dps(const int n, const int m, const int mod){
+    vector<int> dp(m+1, 0);
+    dp[0] = 1;
+    for(int i=1; i<=m; ++i){
+        dp[i]  = dp[i-1];
+        dp[i] %= mod;
+        for(int j=i; j<=n; ++j){
+            dp[j] += dp[j-i];
+            dp[j] %= mod;
+        }
+    }
+    printf("%d\n", dp[m]);
+}
+
+int main(){
+    int n=4;
+    int m=3;
+    int mod=10000;
+    
+    dps(n, m, mod);
+    
+    return 0;
+}
+
+/*
+4
+3
+10000
+*/
