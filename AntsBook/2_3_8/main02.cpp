114e1ce21927993968d5545e65080f6a67e3993a
admiswalker <admiswalker@gmail.com>
2020-01-14 21:49:51 +0900

    udi

--- AntsBook/2_3_8/main02.cpp
+++ AntsBook/2_3_8/main02.cpp
@@ -2,23 +2,21 @@
 using namespace std;
 
 void dps(const int n, const int m, const int M, const vector<int>& vecA){
-    vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
+    vector<int> dp(n+1, 0);
     
     // 1 つも選ばない方法は常に 1 通り
-    for(int i=0; i<=n; ++i){
-        dp[i][0] = 1;
-    }
+    dp[0] = 1;
     
     for(int i=0; i<n; ++i){
         for(int j=1; j<=m; ++j){
             if(j-1-vecA[i] >= 0){
-                dp[i+1][j] = (dp[i+1][j-1] + dp[i][j] - dp[i][j-1-vecA[i]]) % M;
+                dp[j] = (dp[j-1] + dp[j] - dp[j-1-vecA[i]] + M) % M;
             }else{
-                dp[i+1][j] = (dp[i+1][j-1] + dp[i][j]);
+                dp[j] = (dp[j-1] + dp[j]);
             }
         }
     }
-    printf("%d\n", dp[n][m]);
+    printf("%d\n", dp[n]);
 }
 
 int main(){
