8827c47a163d502aae9ad69a796eaf201da4d877
admiswalker <admiswalker@gmail.com>
2020-01-22 22:27:58 +0900

    udi

--- AtCoder/abc_110_d/main.cpp
+++ AtCoder/abc_110_d/main.cpp
@@ -43,26 +43,44 @@ vector<struct fact> factor(uint64 target){
     }
     return vecFact;
 }
-
+/*
 uint64 nHr(const vector<uint>& vecA, const uint r, const uint64 mod){
     
     const uint& n = vecA.size();
-    vector<vector<uint>> dp(n+1, vector<uint>(r+1, 0));
+    vector<vector<uint64>> dp(n+1, vector<uint64>(r+1, 0ull));
     for(uint i=0; i<=n; ++i){
-        dp[i][0] = 1;
+        dp[i][0] = 1ull;
     }
-    printf("1\n");
     
     for(uint i=0; i<n; ++i){
         for(uint j=1; j<=r; ++j){
-            int64 tmp = j-vecA[i]-1ll;
+            int64 tmp = (int64)j-(int64)vecA[i]-(int64)1ll;
             if(tmp>=0){ dp[i+1][j] = (dp[i+1][j-1] + dp[i][j] - dp[i][tmp]) % mod;
             }   else  { dp[i+1][j] = (dp[i+1][j-1] + dp[i][j]); }
         }
     }
     
-    printf("2\n");
     return dp[n][r];
+}//*/
+int dps(const int n, const int m, const int M, const vector<int>& vecA){
+    vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
+    
+    // 1 つも選ばない方法は常に 1 通り
+    for(int i=0; i<=n; ++i){
+        dp[i][0] = 1;
+    }
+    
+    for(int i=0; i<n; ++i){
+        for(int j=1; j<=m; ++j){
+            if(j-1-vecA[i] >= 0){
+                dp[i+1][j] = (dp[i+1][j-1] + dp[i][j] - dp[i][j-1-vecA[i]]) % M;
+            }else{
+                dp[i+1][j] = (dp[i+1][j-1] + dp[i][j]);
+            }
+        }
+    }
+//    printf("%d\n", dp[n][m]);
+    return dp[n][m];
 }
 
 int main(){
@@ -74,19 +92,21 @@ int main(){
     int N, M; cin >> N >> M;
     
     vector<struct fact> vecFact = factor(M);
-//    for(uint64 i=0; i<vecFact.size(); ++i){
-//        printf(" + %lu^%lu", vecFact[i].prime, vecFact[i].num);
-//    }
-//    printf("\n");
-    
-    vector<uint> vecA(vecFact.size());
-    for(uint i=0; i<vecFact.size(); ++i){
-        printf("%lu  ", vecFact[i].num);
-        vecA[i] = vecFact[i].num;
+    printf("%d = ", M);
+    for(uint64 i=0; i<vecFact.size(); ++i){
+        printf(" + %lu^%lu", vecFact[i].prime, vecFact[i].num);
     }
     printf("\n");
     
-    cout << nHr(vecA, N, mod) << endl;
+//    vector<uint> vecA(vecFact.size());
+    vector<int> vecA(vecFact.size());
+    for(uint i=0; i<vecFact.size(); ++i){
+        vecA[i] = vecFact[i].num;
+    }
+    
+//    cout << nHr(vecA, M, mod) << endl;
+//    cout << dps(n, m, M, vecA) << endl;
+    cout << dps(N, M, mod, vecA) << endl;
     
     return 0;
 }
