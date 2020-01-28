53811584551f42f1ebd893054e1156982f90f9c5
admiswalker <admiswalker@gmail.com>
2020-01-28 23:52:05 +0900

    arc_074_d (begin)

---
+++ AtCoder/arc_074_d/main.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef uint32_t uint;
+typedef uint64_t uint64;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    uint N; cin >> N;
+    vector<uint> vecA(3*N);
+    for(uint i=0; i<vecA.size(); ++i){ cin >> vecA[i]; }
+    
+    for(uint i=N; i<2*N; ++i){
+        cin ;
+    }
+    
+    return 0;
+}
