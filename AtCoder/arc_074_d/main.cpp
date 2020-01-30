a6b2217d69ce894842d790ac76388b162b1592a4
admiswalker <admiswalker@gmail.com>
2020-01-31 00:14:47 +0900

    udi

--- AtCoder/arc_074_d/main.cpp
+++ AtCoder/arc_074_d/main.cpp
@@ -1,5 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
+typedef int64_t int64;
 typedef uint32_t uint;
 typedef uint64_t uint64;
 
@@ -10,39 +11,40 @@ int main(){
     uint N; cin >> N;
     vector<uint> vecA(3*N);
     for(uint i=0; i<vecA.size(); ++i){ cin >> vecA[i]; }
-
+    
     // head
-    uint64 sumH=0ull;
-    priority_queue<uint64,vector<uint64>,greater<uint64>> queH; // head
+    int64 sumH=0ull;
+    priority_queue<int64,vector<int64>,greater<int64>> queH; // head
     for(uint i=0; i<N; ++i){ queH.push(vecA[i]); sumH+=vecA[i]; }
     
-    vector<uint64> vecH(N); vecH.resize(0);
+    vector<int64> vecH(N+1); vecH.resize(0);
     vecH.push_back( sumH );
-    for(uint i=N+1; i<2*N; ++i){
+    for(uint i=N; i<2*N; ++i){
         queH.push(vecA[i]);
         sumH -= queH.top(); queH.pop();
         vecH.push_back( sumH );
     }
     
     // tail
-    uint64 sumT=0ull;
-    priority_queue<uint64> queT; // head
+    int64 sumT=0ull;
+    priority_queue<int64> queT; // head
     for(uint i=2*N; i<3*N; ++i){ queT.push(vecA[i]); sumT+=vecA[i]; }
     
-    vector<uint64> vecT(N); vecT.resize(0);
+    vector<int64> vecT(N+1); vecT.resize(0);
     vecT.push_back( sumT );
-    for(uint i=N+1; i<2*N; ++i){
+    for(uint i=2*N-1; i>N; --i){
         queT.push(vecA[i]);
-        sumT -= queT.top();
+        sumT -= queT.top(); queT.pop();
         vecT.push_back( sumT );
     }
     
-    uint64 max=0ull;
-    for(uint i=1; i<vecT.size()-1; ++i){
-        uint64 buf = vecH[i] - vecT[i+1];
+    // solve
+    int64 max=0ull;
+    for(uint i=0; i<N+1; ++i){
+        int64 buf = vecH[i] - vecT[i];
         if(buf>max){ max=buf; }
     }
-    cout << max << endl;
     
+    cout << max << endl;
     return 0;
 }
