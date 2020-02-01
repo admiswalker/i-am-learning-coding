652f44c9882610b3526f9dbc7c793a6a98f05c0c
admiswalker <admiswalker@gmail.com>
2020-02-01 17:31:53 +0900

    AC

--- AtCoder/arc_074_d/main.cpp
+++ AtCoder/arc_074_d/main.cpp
@@ -14,34 +14,36 @@ int main(){
     
     // head
     int64 sumH=0ull;
-    priority_queue<int64,vector<int64>,greater<int64>> queH; // head
+    priority_queue<int64,vector<int64>,greater<int64>> queH;
     for(uint i=0; i<N; ++i){ queH.push(vecA[i]); sumH+=vecA[i]; }
     
     vector<int64> vecH(N+1); vecH.resize(0);
     vecH.push_back( sumH );
     for(uint i=N; i<2*N; ++i){
         queH.push(vecA[i]);
+        sumH += vecA[i];
         sumH -= queH.top(); queH.pop();
         vecH.push_back( sumH );
     }
     
     // tail
     int64 sumT=0ull;
-    priority_queue<int64> queT; // head
+    priority_queue<int64> queT;
     for(uint i=2*N; i<3*N; ++i){ queT.push(vecA[i]); sumT+=vecA[i]; }
     
     vector<int64> vecT(N+1); vecT.resize(0);
     vecT.push_back( sumT );
-    for(uint i=2*N-1; i>N; --i){
+    for(uint i=2*N-1; i>=N; --i){
         queT.push(vecA[i]);
+        sumT += vecA[i];
         sumT -= queT.top(); queT.pop();
         vecT.push_back( sumT );
     }
     
     // solve
-    int64 max=0ull;
+    int64 max=INT64_MIN;
     for(uint i=0; i<N+1; ++i){
-        int64 buf = vecH[i] - vecT[i];
+        int64 buf = vecH[i] - vecT[N-i];
         if(buf>max){ max=buf; }
     }
     
