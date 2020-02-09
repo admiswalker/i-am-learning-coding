facc1db17e53ee79a11a17b6312005f97914dc19
admiswalker <admiswalker@gmail.com>
2020-02-10 00:08:37 +0900

    add abc_126_d udi

---
+++ AtCoder/abc_126_d/main.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef int64_t int64;
+typedef uint32_t uint;
+typedef uint64_t uint64;
+
+struct edge{
+    uint to;
+    uint cost;
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    uint N; cin >> N;
+    vector<uint> graph(N);
+    for(uint i=0; i<N; ++i){
+//        cin >> graph[here].push_back(to, cost);
+    }
+    
+    return 0;
+}
+
+// 100000
+// 100000
+// 10^5 * 10^5 * 1 Byte / (1024*1024)
+// ≒ 10^10 Byte / 10^6
+// ≒ 10^4 MByte
+
