f133de960a40822360197fa061875ad02fd5a9c6
admiswalker <admiswalker@gmail.com>
2020-02-13 00:14:09 +0900

    udi

--- AtCoder/soundhound2018_summer_qual_d/main.cpp
+++ AtCoder/soundhound2018_summer_qual_d/main.cpp
@@ -4,11 +4,13 @@ typedef int64_t int64;
 typedef uint32_t uint;
 typedef uint64_t uint64;
 
+typedef pair<uint64,uint64> P;
+
 struct edge{
 private:
 public:
     edge(){}
-    edge(uint to_in, uint a_in, uint b_in) to(to_in), a(a_in), b(b_in) {}
+    edge(uint to_in, uint a_in, uint b_in): to(to_in), a(a_in), b(b_in) {}
     ~edge(){}
     uint to;
     uint a; // Yen
@@ -20,17 +22,30 @@ uint edge2cost(edge val, uint i, uint n){
     } else { return val.b; }
 }
 
-vector<uint64> dijkstra(uint s, uint n){
-    vector<vector<uint64>> vvec;
-    ;
-    return ;
-}
-vector<vector<uint64>> dijkstra_n(uint s, uint n){
-    vector<vector<uint64>> vvec(n);
-    for(uint i=0; i<n; ++i){
-        vvec[n] = dijkstra(s, i);
+vector<uint64> dijkstra(const vector<vector<edge>>& graph, uint s, uint n){
+    vector<uint64> vCost(graph.size(), UINT64_MAX); vCost[0]=0ull;
+    priority_queue<P,vector<P>,greater<P>> que; que.push(P(0,s));
+    
+    while(!que.empty()){
+        P p = que.top(); que.pop();
+        uint64 cost = p.first;
+        uint64 idx  = p.second;
+        if(vCost[idx] < cost){ continue; }
+        for(uint i=0; i<graph[idx].size(); ++i){
+            edge e = graph[idx][i];
+            if(vCost[e.to] < vCost[idx] + edge2cost(e,idx,n)){ continue; }
+            vCost[e.to] = vCost[idx] + edge2cost(e,idx,n);
+            que.push( P(vCost[e.to], e.to) );
+        }
     }
-    return vvec;
+    return vCost;
+}
+vector<vector<uint64>> dijkstra_n(const vector<vector<edge>>& graph, uint s, uint n){
+    vector<vector<uint64>> vvCost(n);
+    for(uint i=0; i<n; ++i){
+        vvCost[i] = dijkstra(graph, s, i);
+    }
+    return vvCost;
 }
 
 int main(){
@@ -40,14 +55,14 @@ int main(){
     uint n, m, s, t;
     cin >> n >> m >> s >> t;
     
-    vector<edge> graph(m);
+    vector<vector<edge>> graph(m);
     for(uint i=0; i<m; ++i){
         uint u, v, a, b;
         cin >> u >> v >> a >> b; --u; --v;
-        graph[u] = edge(v, a, b);
+        graph[u].push_back( edge(v, a, b) );
     }
     
-    vector<vector<uint64>> vecCost = dijkstra_n(s, n);
+    vector<vector<uint64>> vecCost = dijkstra_n(graph, s, n);
     uint64 prev=vecCost[n-1][t];
     for(int i=n-2; i>=0; --i){
         if(prev>vecCost[i][t]){ vecCost[i][t] = prev; }
