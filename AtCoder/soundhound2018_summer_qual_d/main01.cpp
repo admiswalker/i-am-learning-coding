08a67efa99a00957fb53d63b4370ad6746da5da0
admiswalker <admiswalker@gmail.com>
2020-02-15 23:38:27 +0900

    udi

--- AtCoder/soundhound2018_summer_qual_d/main01.cpp
+++ AtCoder/soundhound2018_summer_qual_d/main01.cpp
@@ -10,16 +10,13 @@ struct edge{
 private:
 public:
     edge(){}
-    edge(uint to_in, uint a_in, uint b_in): to(to_in), a(a_in), b(b_in) {}
+    edge(uint to_in, uint cost_in): to(to_in), cost(cost_in) {}
     ~edge(){}
     uint to;
-    uint a; // Yen
-    uint b; // Snuuk
+    uint cost;
 };
 
-uint edge2cost(edge val, bool ab){ return ab ? val.a : val.b; }
-
-vector<uint64> dijkstra(const vector<vector<edge>>& graph, uint s, bool ab){
+vector<uint64> dijkstra(const vector<vector<edge>>& graph, uint s){
     vector<uint64> vCost(graph.size(), UINT64_MAX); vCost[s]=0ull;
     priority_queue<P,vector<P>,greater<P>> que; que.push( P(0,s) );
     
@@ -30,12 +27,17 @@ vector<uint64> dijkstra(const vector<vector<edge>>& graph, uint s, bool ab){
         if(vCost[idx] < cost){ continue; }
         for(uint i=0; i<graph[idx].size(); ++i){
             edge e = graph[idx][i];
-            if(vCost[e.to] <= vCost[idx] + edge2cost(e, ab)){ continue; }
-            vCost[e.to] = vCost[idx] + edge2cost(e, ab);
+            if(vCost[e.to] <= vCost[idx] + e.cost){ continue; }
+            vCost[e.to] = vCost[idx] + e.cost;
             que.push( P(vCost[e.to], e.to) );
         }
     }
     return vCost;
+}
+
+uint cost_u2v(const vector<vector<edge>>& graph_a, uint u, uint v){
+    vector<uint64> vCost = dijkstra(graph_a, u);
+    return vCost[v];
 }
 
 int main(){
@@ -45,16 +47,26 @@ int main(){
     uint n, m, s, t;
     cin >> n >> m >> s >> t; --s; --t;
     
-    vector<vector<edge>> graph(n);
+    vector<vector<edge>> graph_a(n), graph_b(n);
     for(uint i=0; i<m; ++i){
         uint u, v, a, b;
         cin >> u >> v >> a >> b; --u; --v;
-        graph[u].push_back( edge(v, a, b) );
-        graph[v].push_back( edge(u, a, b) );
+        
+        graph_a[u].push_back( edge(v, a) );
+        graph_b[u].push_back( edge(v, b) );
+        
+        graph_a[v].push_back( edge(u, a) );
+        graph_b[v].push_back( edge(u, b) );
     }
     
-    vector<uint64> ret = dijkstra(graph, s, true);
-    cout << ret[t] << endl;
+    for(uint i=0; i<n; ++i){
+        uint ret;
+        if(i<){
+            ret = cost_u2v(graph_a, s, t);
+        }else{
+        }
+        cout << ret << endl;
+    }
     
     return 0;
 }
