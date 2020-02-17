245641ab9c1c2faf2a8d189b8d94c8ce6a308c23
admiswalker <admiswalker@gmail.com>
2020-02-17 21:34:36 +0900

    udi

--- AtCoder/abc_065_d/main01.cpp
+++ AtCoder/abc_065_d/main01.cpp
@@ -4,17 +4,6 @@ typedef int64_t int64;
 typedef uint32_t uint;
 typedef uint64_t uint64;
 
-struct vertex{
-private:
-public:
-    vertex(){}
-    vertex(uint idx_in, uint x_in, uint y_in): idx(idx_in), x(x_in) y(y_in) {}
-    ~edge(){}
-    uint idx;
-    uint x;
-    uint y;
-};
-
 struct edge{
 private:
 public:
@@ -24,11 +13,11 @@ public:
     uint to;
     uint cost;
 };
-/*
+
 uint64 solve_prime(const vector<vector<edge>>& graph){
     const uint& N = graph.size();
     vector<bool> vUsed(N, false);
-    vector<uint64> vCost_min(N, INT64_MAX); vCost_min[0]=0ull;
+    vector<uint> vCost_min(N, UINT_MAX); vCost_min[0]=0;
     
     uint64 sum_cost=0ull;
     for(;;){
@@ -39,58 +28,58 @@ uint64 solve_prime(const vector<vector<edge>>& graph){
         if(minIdx==-1){ break; }
         
         vUsed[minIdx] = true;
-        sum_cost += vCost_min[minIdx];
+        sum_cost += (uint64)vCost_min[minIdx];
         
-        for(uint i=0; i<N; ++i){
-            vector<uint64> vCost_u2v = vXY2vCost_u2v(vXY, minIdx);
-            vCost_min[i] = min(vCost_min[i], vCost_u2v[i]);
+        for(uint i=0; i<graph[minIdx].size(); ++i){
+            edge e = graph[minIdx][i];
+            vCost_min[e.to] = min(vCost_min[e.to], e.cost);
         }
     }
     return sum_cost;
 }
-//*/
 
-template<typename T>
-bool cmp_by_second(const std::pair<T, T>& lhs, const std::pair<T, T>& rhs) {
-    if(lhs.second != rhs.second){ return lhs.second < rhs.second;
-    }            else           { return lhs.first  < rhs.first;  }
-}
+struct vertex{
+private:
+public:
+    vertex(){}
+    vertex(uint idx_in, uint x_in, uint y_in): idx(idx_in), x(x_in), y(y_in) {}
+    ~vertex(){}
+    uint idx;
+    uint x;
+    uint y;
+};
+bool cmp_x(const struct vertex& lhs, const struct vertex& rhs){ return lhs.x < rhs.x; }
+bool cmp_y(const struct vertex& lhs, const struct vertex& rhs){ return lhs.y < rhs.y; }
+uint64 costAB(const struct vertex lhs, const struct vertex rhs){ return min(abs(lhs.x-rhs.x), abs(lhs.y-rhs.y)); }
 
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     
     uint N; cin >> N;
-    vector<pair<int,int>> vXY(N);
+    vector<vertex> vXY(N);
     for(uint i=0; i<N; ++i){
         uint x, y; cin >> x >> y;
-        vXY[i].first  = x;
-        vXY[i].second = y;
+        vXY[i].idx = i;
+        vXY[i].x   = x;
+        vXY[i].y   = y;
     }
     
-    vector<pair<int,int>> vXY_x = vXY; sort(vXY_x.begin(), vXY_x.end());
-    vector<pair<int,int>> vXY_y = vXY; sort(vXY_y.begin(), vXY_y.end(), cmp_by_second<int>);
+    vector<vertex> vXY_x=vXY; sort(vXY_x.begin(), vXY_x.end(), cmp_x);
+    vector<vertex> vXY_y=vXY; sort(vXY_y.begin(), vXY_y.end(), cmp_y);
     
     vector<vector<edge>> graph(N);
-    vXY_x[i].first  = ;
-    vXY_x[i].second = ;
-    graph[0].emplace_back(0, vXY_x[i].first);
-    for(uint i=0; i<N; ++i){
-        struct edghe buf;
-        buf.to   = ;
-        buf.cost = ;
-        graph[i].emplace_back(buf);
+    for(uint i=0; i<N-1; ++i){
+        uint64 cost_x = costAB(vXY_x[i], vXY_x[i+1]);
+        graph[ vXY_x[i  ].idx ].emplace_back(vXY_x[i+1].idx, cost_x);
+        graph[ vXY_x[i+1].idx ].emplace_back(vXY_x[i  ].idx, cost_x);
+        
+        uint64 cost_y = costAB(vXY_y[i], vXY_y[i+1]);
+        graph[ vXY_y[i  ].idx ].emplace_back(vXY_y[i+1].idx, cost_y);
+        graph[ vXY_y[i+1].idx ].emplace_back(vXY_y[i  ].idx, cost_y);
     }
     
-    /*
-    for(uint i=0; i<N; ++i){
-        cout << vXY[i].first  << endl;
-        cout << vXY[i].second << endl;
-        cout << endl;
-    }
-    //*/
-//    uint64 ret = solve_prime(vXY);
-//    cout << ret << endl;
+    cout << solve_prime(graph) << endl;
     
     return 0;
 }
