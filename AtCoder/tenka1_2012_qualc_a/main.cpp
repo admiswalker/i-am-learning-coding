4647902daf242958e8e716c1d0e93f3203537767
admiswalker <admiswalker@gmail.com>
2020-02-26 23:45:39 +0900

    tenka1_2012_qualc_a

---
+++ AtCoder/tenka1_2012_qualc_a/main.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef int64_t int64;
+typedef uint32_t uint;
+typedef uint64_t uint64;
+
+uint sieve(const uint n){
+    uint p=0;
+    vector<bool> pTable(n, true); pTable[0]=pTable[1]=false;
+    for(uint ti=2; ti<=n; ++ti){
+        if(!pTable[ti]){ continue; }
+        p++;
+        for(uint i=2*ti; i<=n; i+=ti){ pTable[i]=false; }
+    }
+    return p;
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    uint n; cin >> n; n--;
+    cout << sieve(n) << endl;
+    
+    return 0;
+}
