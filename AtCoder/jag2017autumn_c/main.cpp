89737e6c5bae2c7ca27bac586039b7b6e2f945d4
admiswalker <admiswalker@gmail.com>
2020-02-29 17:56:15 +0900

    udi

--- AtCoder/jag2017autumn_c/main.cpp
+++ AtCoder/jag2017autumn_c/main.cpp
@@ -5,42 +5,29 @@ typedef uint32_t uint;
 typedef uint64_t uint64;
 
 uint max(const uint lhs, const uint rhs){ return (lhs>rhs) ? lhs : rhs; }
-/*
-uint sieve_num(uint l, uint r){
-    uint p=0;
-    uint n = sqrt(r) + 1;
-    vector<bool> pTable(n, true); pTable[0]=false; pTable[1]=false;
-    for(uint ti=0; ti<n; ++ti){
-        if(!pTable[ti]){ continue; }
-        ++p;
-        for(uint i=ti*2; i<r; i+=ti){ pTable[i]=false; }
-//        for(uint i=max(2, (ti+l-1)/ti)*ti; i<n; ++i){ pTable[i]=false; }
-    }
-    return p;
+uint countTrue(const vector<bool>& vRhs){
+    uint num=0; for(uint i=0; i<vRhs.size(); ++i){ if(vRhs[i]){++num;} } return num;
 }
-//*/
 
-uint sieve(const uint n){
-    uint p=0;
-//    uint rN = sqrt(n);
-    vector<bool> pTable(n+1, true); pTable[0]=false; pTable[1]=false;
-    for(uint ti=2; ti<=n; ++ti){
+uint sieve_num_lr(const uint l, const uint r){
+    const uint n=r-l+1;
+    const uint rR = sqrt(r)+0.5;
+    vector<bool> pTable   (rR+1, true); pTable[0]=false; pTable[1]=false;
+    vector<bool> pTable_lr( n+1, true);
+    for(uint ti=2; ti<=rR; ++ti){
         if(!pTable[ti]){ continue; }
-        ++p;
-        for(uint i=ti*2; i<n+1; i+=ti){ pTable[i]=false; }
+        for(uint i=ti*2; i<rR+1; i+=ti){ pTable[i]=false; }
+        for(uint i=((l-1)/ti + 1)*ti; i<r+1; i+=ti){ pTable_lr[i-l]=false; }
     }
-    return p;
+    return countTrue(pTable_lr);
 }
 
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     
-    uint r; cin >>  r;
-//    uint l, r; cin >> l >> r;
-//    uint ret = sieve_num(l, r);
-//    uint ret = sieve(l, r);
-    uint ret = sieve(r);
+    uint l, r; cin >> l >> r;
+    uint ret = sieve_num_lr(l, r);
     cout << ret << endl;
     
     return 0;
