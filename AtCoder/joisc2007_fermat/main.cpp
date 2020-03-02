142915ecad86cf7573265ea5f0db82db66601605
admiswalker <admiswalker@gmail.com>
2020-03-03 07:26:22 +0900

    add joisc2007_fermat

---
+++ AtCoder/joisc2007_fermat/main.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef int64_t int64;
+typedef uint32_t uint;
+typedef uint64_t uint64;
+
+uint64 pow_mod(uint64 x, uint64 n, const uint64 mod){
+    uint64 res = 1ull;
+    while(n > 0){
+        if(n & 1){ res = res * x % mod; }
+        x = x * x % mod;
+        n >>= 1;
+    }
+    return res;
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    uint p, n; cin >> p >> n;
+    
+    vector<uint64> vXYZ(p);
+    for(uint i=0; i<p; ++i){ vXYZ[i] = pow_mod(i, n, p); }
+    
+    unordered_map<uint64, uint64> hashT(p);
+    for(uint i=0; i<p; ++i){ hashT[vXYZ[i]] = vXYZ[i]; }
+    
+    uint64 num=0;
+    for(uint x=0; x<p; ++x){
+        for(uint y=0; y<p; ++y){
+            uint64 tmp = (vXYZ[x] + vXYZ[y]) % p;
+            if(hashT.find(tmp)==hashT.end()){ continue; }
+            ++num;
+        }
+    }
+    cout << num << endl;
+    
+    return 0;
+}
