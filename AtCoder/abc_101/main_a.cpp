64d3b2caf18cbaf65536e058f61e59863f70db88
admiswalker <admiswalker@gmail.com>
2020-03-07 15:37:44 +0900

    a AC

--- AtCoder/abc_101/main_a.cpp
+++ AtCoder/abc_101/main_a.cpp
@@ -7,7 +7,16 @@ typedef uint64_t uint64;
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
+
+    string s; cin >> s;
+
+    int num=0;
+    for(uint i=0; i<s.size(); ++i){
+        if(s[i]=='+'){ ++num;
+        }    else    { --num; }
+    }
     
+    cout << num << endl;
     
     return 0;
 }
