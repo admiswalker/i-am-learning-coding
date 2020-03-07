9c25b22052803d6e703a25b3e90f7874280e7209
admiswalker <admiswalker@gmail.com>
2020-03-07 19:03:43 +0900

    c AC

--- AtCoder/abc_101/main_c.cpp
+++ AtCoder/abc_101/main_c.cpp
@@ -17,8 +17,30 @@ int main(){
     }
     
     double n_before = ceil((double)(idx-1)/(double)(K-1));
-    double n_after  = ceil((double)(N-idx)/(double)(K-1));
+    int len_filled = n_before * (K-1) + 1;
+    int len_left = N - len_filled;
+    
+    double n_after  = ceil((double)(len_left)/(double)(K-1));
+    
     cout << (int)(n_before+n_after) << endl;
     
     return 0;
 }
+/*
+case: 4
+3 3
+2 1 3
+>> ans == 1
+*/
+/*
+case: 5
+7 3
+2 1 3 4 5 6 7
+>> ans == 3
+*/
+/*
+case: 6
+8 7
+2 3 4 5 6 7 8 1
+>> ans == 2
+*/
