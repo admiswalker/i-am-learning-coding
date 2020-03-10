9963b27dcbe4a180159bd7878a7111d27c7d2ec9
admiswalker <admiswalker@gmail.com>
2020-03-11 00:17:12 +0900

    udi

--- AtCoder/abc_102/main_d.cpp
+++ AtCoder/abc_102/main_d.cpp
@@ -5,20 +5,20 @@ typedef uint32_t uint;
 typedef uint64_t uint64;
 
 tuple<vector<int>, vector<int>> split2two(const vector<int>& vA){
-    const uint N = vA.size();
+    const int N = vA.size();
     
     vector<int64> sumL(N, 0ll); sumL[0]=vA[0];
-    for(uint i=1; i<N; ++i){ sumL[i]+=vA[i]; }
+    for(int i=1; i<N; ++i){ sumL[i]=sumL[i-1]+vA[i]; }
     
-    vector<int64> sumR(N, 0ll); sumR[0]=vA[N-1];
-    for(uint i=1; i<N; ++i){ sumR[i]+=vA[N-1-i]; }
+    vector<int64> sumR(N, 0ll); sumR[N-1]=vA[N-1];
+    for(int i=N-2; i>=0; --i){ sumR[i]=sumR[i+1]+vA[i]; }
     
     int64 min=INT64_MAX;
     uint minIdxL=0;
-    for(uint i=0; i<N-1; ++i){
+    for(int i=0; i<N-1; ++i){
         int64 diff = abs(sumL[i]-sumR[i+1]);
         if(diff<min){
-            min = abs(sumL[i]-sumR[i+1]);
+            min = diff;
             minIdxL = i;
         }
     }
@@ -26,7 +26,7 @@ tuple<vector<int>, vector<int>> split2two(const vector<int>& vA){
     vector<int> vFront(minIdxL+1);
     vector<int> vRear (N-minIdxL);
     copy(&vA[0], &vA[minIdxL+1], vFront.begin());
-    copy(&vA[minIdxL+1], &vA[N-1]+1, vRear.begin()  );
+    copy(&vA[minIdxL+1], &vA[N], vRear.begin()  );
     
     return tie(vFront, vRear);
 }
@@ -45,12 +45,16 @@ int main(){
     vector<int> vFF, vFR;
     tie(vFF, vFR) = split2two(vF);
     vector<int> vRF, vRR;
-    tie(vRF, vRR) = split2two(vF);
+    tie(vRF, vRR) = split2two(vR);
     
     int64 P = accumulate(vFF.begin(), vFF.end(), 0ll);
     int64 Q = accumulate(vFR.begin(), vFR.end(), 0ll);
     int64 R = accumulate(vRF.begin(), vRF.end(), 0ll);
     int64 S = accumulate(vRR.begin(), vRR.end(), 0ll);
+    cout << "P: " << P << endl;
+    cout << "Q: " << Q << endl;
+    cout << "R: " << R << endl;
+    cout << "S: " << S << endl;
     
     vector<int64> vPQRS = {P, Q, R, S};
     int64 minVal = *min_element(vPQRS.begin(), vPQRS.end());
