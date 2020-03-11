b052af6809b39c3c46d0218c7484506089838c18
admiswalker <admiswalker@gmail.com>
2020-03-11 23:26:54 +0900

    d WA

--- AtCoder/abc_102/main_d.cpp
+++ AtCoder/abc_102/main_d.cpp
@@ -4,9 +4,7 @@ typedef int64_t int64;
 typedef uint32_t uint;
 typedef uint64_t uint64;
 
-tuple<vector<int>, vector<int>> split2two(const vector<int>& vA){
-    const int N = vA.size();
-    
+tuple<int64, int64> split2two(const int* vA, const int N){
     int64 sum=0ll; sum=vA[0];
     vector<int64> vSum(N); vSum[0]=vA[0];
     for(int i=1; i<N; ++i){
@@ -24,12 +22,9 @@ tuple<vector<int>, vector<int>> split2two(const vector<int>& vA){
         }
     }
     
-    vector<int> vFront(   minIdxL+1 );
-    vector<int> vRear (N-(minIdxL+1));
-    copy(&vA[0], &vA[minIdxL+1], vFront.begin());
-    copy(&vA[minIdxL+1], &vA[N], vRear.begin());
-    
-    return tie(vFront, vRear);
+    int64 sumL = accumulate(&vA[0], &vA[minIdxL+1], 0ll);
+    int64 sumR = accumulate(&vA[minIdxL+1], &vA[N], 0ll);
+    return tie(sumL, sumR);
 }
 
 int main(){
@@ -40,52 +35,22 @@ int main(){
     vector<int> vA(N);
     for(uint i=0; i<N; ++i){ cin>>vA[i]; }
     
-    vector<int> vF, vR;
-    tie(vF, vR) = split2two(vA);
-    cout << vF.size() << endl;
-    cout << vR.size() << endl;
-    
-    vector<int> vFF, vFR;
-    tie(vFF, vFR) = split2two(vF);
-    vector<int> vRF, vRR;
-    tie(vRF, vRR) = split2two(vR);
-    
-    int64 P = accumulate(vFF.begin(), vFF.end(), 0ll);
-    int64 Q = accumulate(vFR.begin(), vFR.end(), 0ll);
-    int64 R = accumulate(vRF.begin(), vRF.end(), 0ll);
-    int64 S = accumulate(vRR.begin(), vRR.end(), 0ll);
-    cout << "P: " << P << endl;
-    cout << "Q: " << Q << endl;
-    cout << "R: " << R << endl;
-    cout << "S: " << S << endl;
-    
-    vector<int64> vPQRS = {P, Q, R, S};
-    int64 minVal = *min_element(vPQRS.begin(), vPQRS.end());
-    int64 maxVal = *max_element(vPQRS.begin(), vPQRS.end());
-    
-    cout << maxVal-minVal << endl;
-    
-    /*
-    if(vF.size()==1){
-        // split vR to 3 pieces
+    int64 min=INT64_MAX;
+    for(uint c=2; c<N-2; ++c){ // c: center
+        int64 sumLL, sumLR;
+        tie(sumLL, sumLR) = split2two(&vA[0], c); // [0, C) // size==C
+        
+        int64 sumRL, sumRR;
+        tie(sumRL, sumRR) = split2two(&vA[c], N-c); // [C, N) // size==N-C
+        
+        vector<int64> vPQRS = {sumLL, sumLR, sumRL, sumRR};
+        int64 minVal = *min_element(vPQRS.begin(), vPQRS.end());
+        int64 maxVal = *max_element(vPQRS.begin(), vPQRS.end());
+        if(maxVal-minVal<min){
+            min = maxVal-minVal;
+        }
     }
-    if(vR.size()==1){
-        // split vF to 3 pieces
-    }
-    //*/
+    cout << min << endl;
+    
     return 0;
 }
-
-
-/*
-
-10
-10  71  84  33   6  47  23  25  52  64
-10  81 165 198 204 251 274
-               217 211 164 141 116  64
-                  |
-10  71  84  33   6| 47  23  25  52  64
-10  81 165 198 204| 47  70  95  
-       123  39   6|        211 116  64
-
-*/
