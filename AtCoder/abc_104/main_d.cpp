325dcc4213382e86b4c5dc411094315625ce1eb0
admiswalker <admiswalker@gmail.com>
2020-03-15 22:14:26 +0900

    add html

---
+++ AtCoder/abc_104/main_d.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef int64_t int64;
+typedef uint32_t uint;
+typedef uint64_t uint64;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    
+    return 0;
+}
