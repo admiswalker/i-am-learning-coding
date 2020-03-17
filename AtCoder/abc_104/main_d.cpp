5ba6a1aa09e2013efe7459ff7c3bddef10219eb8
admiswalker <admiswalker@gmail.com>
2020-03-17 22:22:44 +0900

    d AC (copy ref)

--- AtCoder/abc_104/main_d.cpp
+++ AtCoder/abc_104/main_d.cpp
@@ -1,3 +1,6 @@
+// https://img.atcoder.jp/abc104/editorial.pdf
+// https://atcoder.jp/contests/abc104/submissions/2955456
+
 #include <bits/stdc++.h>
 using namespace std;
 typedef int64_t int64;
@@ -8,6 +11,26 @@ int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     
+    const int64 MOD = 1000000007;
+    string s; cin >> s;
+    int N = s.size();
+    
+    vector<vector<int64>> dp(N+1, vector<int64>(4LL));
+    
+    for(int i=N; i>=0; --i){
+        for(int j=3; j>=0; --j){
+            if(i==N){
+                dp[i][j] = (j<3 ? 0LL:1LL);
+            }else{
+                dp[i][j] = dp[i+1][j] * (s[i]=='?' ? 3LL:1LL);
+                if(j<3 && (s[i]=='?' || s[i]=="ABC"[j])){
+                    dp[i][j] += dp[i+1][j+1];
+                }
+                dp[i][j] %= MOD;
+            }
+        }
+    }
+    cout << dp[0][0] << endl;
     
     return 0;
 }
