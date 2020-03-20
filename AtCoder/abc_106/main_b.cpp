10f2e8a514d58d4bdabc02237b3786aaaf331d90
admiswalker <admiswalker@gmail.com>
2020-03-20 18:28:40 +0900

    b WA

---
+++ AtCoder/abc_106/main_b.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef int64_t int64;
+typedef uint32_t uint;
+typedef uint64_t uint64;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    uint N; cin >> N;
+    cout << (N>=105 ? 1:0) << endl;
+    
+    return 0;
+}
