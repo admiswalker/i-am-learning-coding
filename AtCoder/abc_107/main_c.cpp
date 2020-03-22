41edcfc2a837261a126b6ccbde76cae83a1e31f5
admiswalker <admiswalker@gmail.com>
2020-03-22 16:02:36 +0900

    c AC

--- AtCoder/abc_107/main_c.cpp
+++ AtCoder/abc_107/main_c.cpp
@@ -29,38 +29,27 @@ int main(){
     }
     vXm.push_back(0);
     reverse(vXm.begin(), vXm.end());
-    printn(vXm);
-    printn(vXp);
-    
-    vector<int64> vXms(vXm.size()); vXms[0]=0ll; // sum
-    for(uint i=1; i<vXms.size(); ++i){ vXms[i] = vXm[i] + vXms[i-1]; }
-    vector<int64> vXps(vXp.size()); vXps[0]=0ll; // sum
-    for(uint i=1; i<vXps.size(); ++i){ vXps[i] = vXp[i] + vXps[i-1]; }
-    printn(vXms);
-    printn(vXps);
     
     int64 time=INT64_MAX;
     for(uint i=0; i<K; ++i){
-        if(i>=vXms.size()){ continue; }
-        if(K-i>=vXps.size()){ continue; }
-        cout << i << endl;
+        if(  i>=vXm.size()){ continue; }
+        if(K-i>=vXp.size()){ continue; }
         int64 time_m=0;
         int64 time_p=0;
-        if(i<vXms.size()){ time_m = vXms[i] - vXms[0]; }
-        time_p = vXps[K-i] - vXps[0];
+        if(i<vXm.size()){ time_m = vXm[i]; }
+        time_p = vXp[K-i] - vXp[0];
         time = min(time, time_m*2 + time_p);
-        cout << i << ", " << time << endl;
     }
-    cout << time << endl;
     for(uint i=0; i<K; ++i){
-        if(i>=vXps.size()){ continue; }
-        if(K-i>=vXms.size()){ continue; }
+        if(  i>=vXp.size()){ continue; }
+        if(K-i>=vXm.size()){ continue; }
         int64 time_p=0;
         int64 time_m=0;
-        if(i<vXps.size()){ time_p = vXps[i] - vXps[0]; }
-        time_p = vXms[K-i] - vXms[0];
+        if(i<vXp.size()){ time_p = vXp[i]; }
+        time_m = vXm[K-i];
         time = min(time, time_p*2 + time_m);
     }
+    
     cout << time << endl;
     
     return 0;
