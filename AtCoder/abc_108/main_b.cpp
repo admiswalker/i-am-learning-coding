5647828b91808b680757101bbe9d390833d6756e
admiswalker <admiswalker@gmail.com>
2020-03-29 14:20:55 +0900

    b AC

---
+++ AtCoder/abc_108/main_b.cpp
@@ -0,0 +1,53 @@
+//#define _GLIBCXX_DEBUG
+#include <bits/stdc++.h>
+using namespace std;
+typedef int64_t int64;
+typedef uint32_t uint;
+typedef uint64_t uint64;
+
+//---
+
+template <typename T>
+inline void print(const T& rhs){ std::cout<<" = "<<rhs<<std::endl; }
+template <typename T>
+inline void print(const std::vector<T>& rhs){
+    std::cout<<" = [ ";
+    for(uint i=0; i<rhs.size(); ++i){ std::cout<<rhs[i]<<' '; }
+    std::cout<<"]"<<std::endl;
+}
+template <typename T>
+inline void print(const std::vector<std::vector<T>>& rhs){
+    std::cout<<" = "<<std::endl;
+    std::cout<<"[[ ";
+    for(uint p=0; p<rhs.size(); ++p){
+        if(p!=0){ std::cout<<" [ "; }
+        for(uint q=0; q<rhs[p].size(); ++q){
+            std::cout<<rhs[p][q]<<' ';
+        }
+        if(p!=rhs.size()-1){ std::cout<<"]"<<std::endl; }
+    }
+    std::cout<<"]]"<<std::endl;
+}
+#define printn(var) {printf("%s", #var);print(var);}
+#define printn_all(var) {printf("%s(%d): ", __func__, __LINE__);printf("%s", #var);print(var);}
+
+//---
+
+int main(){
+//    ios_base::sync_with_stdio(false);
+//    cin.tie(NULL);
+    
+    int x1, y1, x2, y2; cin >> x1 >> y1 >> x2 >> y2;
+    int x3, y3, x4, y4;
+    
+    int a = x2 - x1;
+    int b = y2 - y1;
+    x3 = x2 - b;
+    y3 = y2 + a;
+    x4 = x1 - b;
+    y4 = y1 + a;
+    
+    cout << x3 << ' ' << y3 << ' ' << x4 << ' ' << y4 << endl;
+    
+    return 0;
+}
