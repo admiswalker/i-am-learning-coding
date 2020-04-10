f405140c947612eccc57467bfb1da2d647a8dffe
admiswalker <admiswalker@gmail.com>
2020-04-11 01:33:54 +0900

    udi

---
+++ AtCoder/abc_114/main_c.cpp
@@ -0,0 +1,56 @@
+//#define _GLIBCXX_DEBUG
+#include <bits/stdc++.h>
+using namespace std;
+typedef int64_t int64;
+typedef uint32_t uint;
+typedef uint64_t uint64;
+
+//---
+
+template <typename T>
+inline void print(const T& rhs){ std::cout<<" = "<<rhs<<std::endl; }
+template <typename T>
+inline void print(const std::vector<T>& rhs){
+    std::cout<<" = [ ";
+    for(uint i=0; i<rhs.size(); ++i){ std::cout<<rhs[i]<<' '; }
+    std::cout<<"]"<<std::endl;
+}
+template <typename T>
+inline void print(const std::vector<std::vector<T>>& rhs){
+    std::cout<<" = "<<std::endl;
+    std::cout<<"[[ ";
+    for(uint p=0; p<rhs.size(); ++p){
+        if(p!=0){ std::cout<<" [ "; }
+        for(uint q=0; q<rhs[p].size(); ++q){
+            std::cout<<rhs[p][q]<<' ';
+        }
+        if(p!=rhs.size()-1){ std::cout<<"]"<<std::endl; }
+    }
+    std::cout<<"]]"<<std::endl;
+}
+template <typename TL, typename TR>
+inline void print(const std::vector<std::pair<TR,TL>>& rhs){
+    std::cout<<" = [";
+    uint i=0;
+    for(; i<rhs.size()-1; ++i){ std::cout<<"[f: "<<rhs[i].first<<", s: "<<rhs[i].second<<"], "; }
+    std::cout<<"[f: "<<rhs[i].first<<", s: "<<rhs[i].second<<"]]" << endl;
+}
+#define printn(var) {printf("%s", #var);print(var);}
+#define printn_all(var) {printf("%s(%d): ", __func__, __LINE__);printf("%s", #var);print(var);}
+
+//---
+
+int main(){
+//    ios_base::sync_with_stdio(false);
+//    cin.tie(NULL);
+    
+    string s; cin >> s;
+    int ans=INT_MAX;
+    for(uint b=0; b<=s.size()-3; ++b){ // b: begin
+        int tmp = 100*(s[b+0]-'0') + 10*(s[b+1]-'0') + (s[b+2]-'0');
+        ans = min(ans, abs(tmp-753));
+    }
+    cout << ans << endl;
+    
+    return 0;
+}
