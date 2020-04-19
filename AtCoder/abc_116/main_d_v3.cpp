76d5cf7a52848c58ddcc15aaada7412a90bb77db
admiswalker <admiswalker@gmail.com>
2020-04-19 21:42:13 +0900

    d AC

--- AtCoder/abc_116/main_d_v3.cpp
+++ AtCoder/abc_116/main_d_v3.cpp
@@ -49,8 +49,8 @@ bool cmp_d   (struct td& lhs, struct td& rhs){ return lhs.d < rhs.d; }
 bool cmp_gr_d(struct td& lhs, struct td& rhs){ return lhs.d > rhs.d; }
 
 int main(){
-//    ios_base::sync_with_stdio(false);
-//    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
     
     uint N, K; cin >> N >> K;
     vector<struct td> vTD(N); for(uint i=0; i<N; ++i){ cin >> vTD[i].t >> vTD[i].d; }
@@ -66,22 +66,23 @@ int main(){
         ++htUN[ vTD[i].t ];
         if(htUN[ vTD[i].t ]>=2){ vTD_rm.push_back( vTD[i] ); }
     }
-    sort(vTD_rm.begin(), vTD_rm.end(), cmp_d);
+//    sort(vTD_rm.begin(), vTD_rm.end(), cmp_d); // dont need to sort. By reversing the iteration to vTD_rm.
     
     for(uint i=K; i<N; ++i){
         if( htUN[ vTD[i].t ] != 0 ){ continue; }
         vTD_add.push_back( vTD[i] );
     }
-    sort(vTD_add.begin(), vTD_add.end(), cmp_gr_d);
+//    sort(vTD_add.begin(), vTD_add.end(), cmp_gr_d); // dont need to sort. Because of the vTD is already sorted.
     
     int64 numT = K - vTD_rm.size(); // num of type
     int64 point = sum + numT*numT;
-    uint ri=0, ai=0;
-    while( ri<vTD_rm.size() && ai<vTD_add.size() ){
-        if( htUN[ vTD_rm [ri].t ] == 1 ){ ++ri; continue; }
+    int ri=vTD_rm.size()-1;
+    int ai=0;
+    while( ri>=0 && ai<vTD_add.size() ){
+        if( htUN[ vTD_rm [ri].t ] == 1 ){ --ri; continue; }
         if( htUN[ vTD_add[ai].t ] >= 1 ){ ++ai; continue; }
         
-        sum -= vTD_rm [ri].d; --htUN[ vTD_rm [ri].t ]; ++ri;
+        sum -= vTD_rm [ri].d; --htUN[ vTD_rm [ri].t ]; --ri;
         sum += vTD_add[ai].d; ++htUN[ vTD_add[ai].t ]; ++ai;
         
         ++numT;
