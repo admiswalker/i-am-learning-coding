30f46d08bd09d518347464eb24df517c2e43120a
admiswalker <admiswalker@gmail.com>
2020-04-22 21:42:08 +0900

    b AC

--- AtCoder/abc_119/main_b.cpp
+++ AtCoder/abc_119/main_b.cpp
@@ -1,24 +1,17 @@
-// ref: https://atcoder.jp/contests/abc119/tasks/abc119_a
-
 #include <bits/stdc++.h>
 typedef int64_t int64;
 using namespace std;
 
 int main(){
+    double sum=0.0;
     
     int N; cin >> N;
-    for(int i=0; i<; ++i){
-        int x; string s; cin >> x >> s;
-        
+    for(int i=0; i<N; ++i){
+        double x; string s; cin >> x >> s;
+        if(s=="BTC"){ x *= 380000.0; }
+        sum += x;
     }
-    int64 date_s  = y*10000 + m*100 + d;
-    int64 date_th = 2019*10000 + 4*100 + 30;
-    
-    if(date_s <= date_th){
-        cout << "Heisei" << endl;
-    }else{
-        cout << "TBD" << endl;
-    }
+    cout << sum << endl;
     
     return 0;
 }
