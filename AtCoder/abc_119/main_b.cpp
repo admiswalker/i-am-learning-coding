0dae9cb579fd1b57b7007f38e0b8d475619df794
admiswalker <admiswalker@gmail.com>
2020-04-22 17:40:38 +0900

    abc_119 a AC

---
+++ AtCoder/abc_119/main_b.cpp
@@ -0,0 +1,24 @@
+// ref: https://atcoder.jp/contests/abc119/tasks/abc119_a
+
+#include <bits/stdc++.h>
+typedef int64_t int64;
+using namespace std;
+
+int main(){
+    
+    int N; cin >> N;
+    for(int i=0; i<; ++i){
+        int x; string s; cin >> x >> s;
+        
+    }
+    int64 date_s  = y*10000 + m*100 + d;
+    int64 date_th = 2019*10000 + 4*100 + 30;
+    
+    if(date_s <= date_th){
+        cout << "Heisei" << endl;
+    }else{
+        cout << "TBD" << endl;
+    }
+    
+    return 0;
+}
