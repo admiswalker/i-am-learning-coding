11ebb702e13a86ca148f7129df29d5114a7f88d9
admiswalker <admiswalker@gmail.com>
2020-04-25 23:21:45 +0900

    d WA

--- AtCoder/abc_119/main_d.cpp
+++ AtCoder/abc_119/main_d.cpp
@@ -40,16 +40,16 @@ inline void print(const std::vector<std::pair<TR,TL>>& rhs){
 
 //---
 
-int nearest_idx(const vector<int64>& v, int64 dst){
+int arg_nearest(const vector<int64>& v, int64 dst){
     int l_idx = 0;
     int r_idx = v.size() - 1;
     
     if(dst <= v[l_idx]){ return l_idx; }
     if(dst >= v[r_idx]){ return r_idx; }
     while(r_idx - l_idx > 1){ // binary search
-        int next = (l_idx + r_idx)/2;
-        if(dst < v[next]){ r_idx = next;
-        }      else      { l_idx = next; }
+        int n_idx = (l_idx + r_idx)/2;
+        if(dst <= v[n_idx]){ r_idx = n_idx;
+        }      else       { l_idx = n_idx; }
     }
     return (abs(v[l_idx]-dst) < abs(v[r_idx]-dst) ? l_idx : r_idx); // return neatest index with value.
 }
@@ -59,20 +59,20 @@ int main(){
 //    cin.tie(NULL);
     
     int A, B, Q; cin >> A >> B >> Q;
-    vector<int64> vS(A); for(int i=0; i<A; ++i){ cin>>vS[i]; }
-    vector<int64> vT(B); for(int i=0; i<B; ++i){ cin>>vT[i]; }
+    vector<int64> vS(A); for(int i=0; i<A; ++i){ cin>>vS[i]; } sort(vS.begin(), vS.end());
+    vector<int64> vT(B); for(int i=0; i<B; ++i){ cin>>vT[i]; } sort(vT.begin(), vT.end());
     vector<int64> vX(Q); for(int i=0; i<Q; ++i){ cin>>vX[i]; }
     
     for(int i=0; i<Q; ++i){
         int idx1, idx2;
         
         int64 tmp1=0;
-        idx1 = nearest_idx(vS, vX[i]   ); tmp1 += abs(vS[idx1] - vX[i]   );
-        idx2 = nearest_idx(vT, vS[idx1]); tmp1 += abs(vT[idx2] - vS[idx1]);
+        idx1 = arg_nearest(vS, vX[i]   ); tmp1 += abs(vS[idx1] - vX[i]   );
+        idx2 = arg_nearest(vT, vS[idx1]); tmp1 += abs(vT[idx2] - vS[idx1]);
         
         int64 tmp2=0;
-        idx1 = nearest_idx(vT, vX[i]   ); tmp2 += abs(vT[idx1] - vX[i]   );
-        idx2 = nearest_idx(vS, vT[idx1]); tmp2 += abs(vS[idx2] - vT[idx1]);
+        idx1 = arg_nearest(vT, vX[i]   ); tmp2 += abs(vT[idx1] - vX[i]   );
+        idx2 = arg_nearest(vS, vT[idx1]); tmp2 += abs(vS[idx2] - vT[idx1]);
         
         cout << min(tmp1, tmp2) << endl;
     }
