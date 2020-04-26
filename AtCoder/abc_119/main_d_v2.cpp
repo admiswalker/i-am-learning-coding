691c770e55cc4564b305a156cacd122c1df281b4
admiswalker <admiswalker@gmail.com>
2020-04-26 14:46:23 +0900

    add d v2

--- AtCoder/abc_119/main_d_v2.cpp
+++ AtCoder/abc_119/main_d_v2.cpp
@@ -2,10 +2,12 @@
 
 //#define _GLIBCXX_DEBUG
 #include <bits/stdc++.h>
-using namespace std;
+
 typedef int64_t int64;
 typedef uint32_t uint;
 typedef uint64_t uint64;
+
+using namespace std;
 
 //---
 
@@ -41,7 +43,7 @@ inline void print(const std::vector<std::pair<TR,TL>>& rhs){
 #define printn_all(var) {printf("%s(%d): ", __func__, __LINE__);printf("%s", #var);print(var);}
 
 //---
-
+/*
 int64 nearest(const vector<int64>& v, int64 dst){
     int l_idx = 0;
     int r_idx = v.size() - 1;
@@ -53,7 +55,7 @@ int64 nearest(const vector<int64>& v, int64 dst){
         if(dst < v[n_idx]){ r_idx = n_idx;
         }      else       { l_idx = n_idx; }
     }
-    return (abs(v[l_idx]-dst) < abs(v[r_idx]-dst) ? v[l_idx] : v[r_idx]);
+    return (abs(v[l_idx]-dst) < abs(v[r_idx]-dst) ? v[l_idx] : v[r_idx]); // return neatest index with value.
 }
 pair<int,int> arg_nearest(const vector<int64>& v, int64 dst){
     int l_idx = 0;
@@ -67,6 +69,33 @@ pair<int,int> arg_nearest(const vector<int64>& v, int64 dst){
         }      else       { l_idx = n_idx; }
     }
     return make_pair(l_idx, r_idx);
+}
+//*/
+template<typename T>
+T nearest_down(const vector<T>& v, const T& value){
+    uint idx = std::upper_bound(v.begin(), v.end(), value) - v.begin();
+    return v[ (idx!=0 ? idx-1:0) ];
+}
+template<typename T>
+T nearest_up  (const vector<T>& v, const T& value){
+    uint idx = std::lower_bound(v.begin(), v.end(), value) - v.begin();
+    return v[ (idx!=v.size() ? idx:v.size()-1) ];
+}
+template<typename T>
+T nearest(const vector<T>& v, const T& value){
+    T val1 = nearest_down(v, value);
+    T val2 = nearest_up  (v, value);
+    return (abs(val1-value)<abs(val2-value) ? val1 : val2);
+}
+template<typename T>
+uint argnearest_down(const vector<T>& v, const T& value){
+    uint idx = upper_bound(v.begin(), v.end(), value) - v.begin();
+    return (idx!=0 ? idx-1:0);
+}
+template<typename T>
+uint argnearest_up(const vector<T>& v, const T& value){
+    uint idx = lower_bound(v.begin(), v.end(), value) - v.begin();
+    return (idx!=v.size() ? idx:v.size()-1);
 }
 
 int main(){
@@ -82,27 +111,27 @@ int main(){
         int idxL, idxR;
         int64 val1, val2;
         
-        int64 tmp1=0ll, tmp2=0ll;
-        tie(idxL, idxR) = arg_nearest(vS, vX[i]);
+        idxL = argnearest_down(vS, vX[i]);
+        idxR = argnearest_up  (vS, vX[i]);
         val1 = nearest(vT, vS[idxL]);
         val2 = nearest(vT, vS[idxR]);
+        int64 tmp1=0ll, tmp2=0ll;
         tmp1 += abs(vS[idxL] - vX[i]   );
         tmp1 += abs( val1    - vS[idxL]);
         tmp2 += abs(vS[idxR] - vX[i]   );
         tmp2 += abs( val2    - vS[idxR]);
-        tmp1 = min(tmp1, tmp2);
         
-        int64 tmp3=0ll, tmp4=0ll;
-        tie(idxL, idxR) = arg_nearest(vT, vX[i]);
+        idxL = argnearest_down(vT, vX[i]);
+        idxR = argnearest_up  (vT, vX[i]);
         val1 = nearest(vS, vT[idxL]);
         val2 = nearest(vS, vT[idxR]);
+        int64 tmp3=0ll, tmp4=0ll;
         tmp3 += abs(vT[idxL] - vX[i]   );
         tmp3 += abs( val1    - vT[idxL]);
         tmp4 += abs(vT[idxR] - vX[i]   );
         tmp4 += abs( val2    - vT[idxR]);
-        tmp3 = min(tmp3, tmp4);
         
-        cout << min(tmp1, tmp3) << endl;
+        cout << min(min(tmp1, tmp2), min(tmp3, tmp4)) << endl;
     }
     
     return 0;
