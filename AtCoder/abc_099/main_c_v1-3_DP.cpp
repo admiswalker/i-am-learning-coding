15dec466930b971bbd4d9ec8562015e16d58666a
admiswalker <admiswalker@gmail.com>
2020-05-06 17:07:11 +0900

    c AC, v1-3_DP

---
+++ AtCoder/abc_099/main_c_v1-3_DP.cpp
@@ -0,0 +1,60 @@
+// ref: https://qiita.com/drken/items/ace3142967c4f01d42e9, 1-3: DP
+
+//#define _GLIBCXX_DEBUG
+#include <bits/stdc++.h>
+typedef int64_t int64;
+typedef uint32_t uint;
+typedef uint64_t uint64;
+using namespace std;
+
+//---
+
+template <typename T>
+inline void print(const T& rhs){ std::cout<<" = "<<rhs<<std::endl; }
+template <typename T>
+inline void print(const std::vector<T>& rhs){
+    std::cout<<" = [ ";
+    for(uint i=0; i<rhs.size(); ++i){ std::cout<<rhs[i]<<' '; }
+    std::cout<<"]"<<std::endl;
+}
+template <typename T>
+inline void print(const std::vector<std::vector<T>>& rhs){
+    std::cout<<" = "<<std::endl;
+    std::cout<<"[[ ";
+    for(uint p=0; p<rhs.size(); ++p){
+        if(p!=0){ std::cout<<" [ "; }
+        for(uint q=0; q<rhs[p].size(); ++q){
+            std::cout<<rhs[p][q]<<' ';
+        }
+        if(p!=rhs.size()-1){ std::cout<<"]"<<std::endl; }
+    }
+    std::cout<<"]]"<<std::endl;
+}
+template <typename TL, typename TR>
+inline void print(const std::vector<std::pair<TR,TL>>& rhs){
+    std::cout<<" = [";
+    uint i=0;
+    for(; i<rhs.size()-1; ++i){ std::cout<<"[f: "<<rhs[i].first<<", s: "<<rhs[i].second<<"], "; }
+    std::cout<<"[f: "<<rhs[i].first<<", s: "<<rhs[i].second<<"]]" << endl;
+}
+#define printn(var) {printf("%s", #var);print(var);}
+#define printn_all(var) {printf("%s(%d): ", __func__, __LINE__);printf("%s", #var);print(var);}
+
+//---
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int N; cin >> N;
+    vector<int> dp(N+1, INT_MAX); dp[0]=0;
+    
+    for(int n=0; n<=N; ++n){
+        for(int pow6=1; n+pow6<=N; pow6*=6){ dp[n+pow6]=min(dp[n+pow6], dp[n]+1); }
+        for(int pow9=1; n+pow9<=N; pow9*=9){ dp[n+pow9]=min(dp[n+pow9], dp[n]+1); }
+    }
+    
+    cout << dp[N] << endl;
+    
+    return 0;
+}
