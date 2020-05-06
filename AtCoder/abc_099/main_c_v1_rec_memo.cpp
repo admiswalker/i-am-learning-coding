4d6a17b9f647d8a80763d5bbe2d3e56424422368
admiswalker <admiswalker@gmail.com>
2020-05-06 15:38:39 +0900

    c v1 AC

---
+++ AtCoder/abc_099/main_c_v1_rec_memo.cpp
@@ -0,0 +1,67 @@
+// ref: https://qiita.com/drken/items/ace3142967c4f01d42e9, v1: rec memo
+
+//#define _GLIBCXX_DEBUG
+#include <bits/stdc++.h>
+typedef int64_t int64;
+typedef uint32_t uint;
+typedef uint64_t uint64;
+using namespace std;
+
+//---
+
+template <typename T>
+inline void print(const T& rhs){ std::cout<<" = "<<rhs<<std::endl; }
+template <typename T>
+inline void print(const std::vector<T>& rhs){
+    std::cout<<" = [ ";
+    for(uint i=0; i<rhs.size(); ++i){ std::cout<<rhs[i]<<' '; }
+    std::cout<<"]"<<std::endl;
+}
+template <typename T>
+inline void print(const std::vector<std::vector<T>>& rhs){
+    std::cout<<" = "<<std::endl;
+    std::cout<<"[[ ";
+    for(uint p=0; p<rhs.size(); ++p){
+        if(p!=0){ std::cout<<" [ "; }
+        for(uint q=0; q<rhs[p].size(); ++q){
+            std::cout<<rhs[p][q]<<' ';
+        }
+        if(p!=rhs.size()-1){ std::cout<<"]"<<std::endl; }
+    }
+    std::cout<<"]]"<<std::endl;
+}
+template <typename TL, typename TR>
+inline void print(const std::vector<std::pair<TR,TL>>& rhs){
+    std::cout<<" = [";
+    uint i=0;
+    for(; i<rhs.size()-1; ++i){ std::cout<<"[f: "<<rhs[i].first<<", s: "<<rhs[i].second<<"], "; }
+    std::cout<<"[f: "<<rhs[i].first<<", s: "<<rhs[i].second<<"]]" << endl;
+}
+#define printn(var) {printf("%s", #var);print(var);}
+#define printn_all(var) {printf("%s(%d): ", __func__, __LINE__);printf("%s", #var);print(var);}
+
+//---
+
+int rec_memo(vector<int>& vMemo, int n){
+    if(n==0){ return 0; }
+    if(vMemo[n] != -1){ return vMemo[n]; }
+    
+    int steps=INT_MAX;
+    
+    for(int pow6=1; pow6<=n; pow6*=6){ steps=min(steps, rec_memo(vMemo, n-pow6)+1);}
+    for(int pow9=1; pow9<=n; pow9*=9){ steps=min(steps, rec_memo(vMemo, n-pow9)+1);}
+    
+    return vMemo[n] = steps;
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int N; cin >> N;
+    vector<int> vMemo(N+1, -1);
+    
+    cout << rec_memo(vMemo, N) << endl;
+    
+    return 0;
+}
